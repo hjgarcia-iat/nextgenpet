8dfcd4bb853bc4bb8c663534e41267e0aacffc93
Henry Garcia <hjgarcia1@gmail.com>
2016-09-12 14:00:38 -0400

    fixed slider

--- resources/views/home/_slider.blade.php
+++ resources/views/home/_slider.blade.php
@@ -2,7 +2,7 @@
     <div id="myCarousel" class="carousel slide">
         <div class="carousel-inner">
             <div class="active item">
-                <div class="fill" style="background-image:url('//placehold.it/1920x500');">
+                <div class="fill" style="background-image:url('../img/slider.jpg');">
                     <div class="container"></div>
                 </div>
             </div>
