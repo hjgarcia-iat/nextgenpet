c0cd1f8fbdedff1977da533eeb68541e3f4fbc43
Henry Garcia <hjgarcia1@gmail.com>
2016-09-14 11:54:58 -0400

    sticky footer created

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,8 +1,17 @@
-<div class="container">
-    <section class="page-footer">
-        <div class="col-md-12">
-            <p class="text-muted text-center"><sup>&copy;</sup> It's About Time {{ date('Y') }}. All Rights Reserved.
-            </p>
+
+
+<section class="page-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="page-footer-content">
+                    <p class="text-muted text-center">
+                    <sup>&copy;</sup> It's About Time {{ date('Y') }}. All Rights Reserved.
+                    </p>
+                </div>
+                
+                
+            </div>
         </div>
-    </section>
-</div>
+    </div>
+</section>
