1ff64e3869a84f0c90b45e5a66cf413e39b70a2c
Henry Garcia <hjgarcia1@gmail.com>
2016-09-14 13:56:26 -0400

    placed all js in one file

--- resources/views/layouts/partials/scripts.blade.php
+++ resources/views/layouts/partials/scripts.blade.php
@@ -3,35 +3,3 @@
 @else
     <script src="{{ elixir('js/all.js') }}"></script>
 @endif
-
-
-<script>
-    $(function () {
-        $('#page-navbar').affix({
-            offset: {
-                top: $('.top-row').height()
-            }
-        });
-    
-        $('#menu').affix({
-            offset: {
-                top: 543
-            }
-        });
-    
-        
-        $('#menu a[data-file="true"]').on('click', function (e) {
-            e.preventDefault();
-            
-            var index = $(this).index();
-
-            $(this).parent('.list-group').find('a.active').removeClass('active');
-            $(this).addClass('active');
-        
-            $('#html-content').hide().siblings('#pdf-content').show();
-            $('#pdf-content').html('<h3>' + $(this).text() + '</h3><iframe class="iframe" src="' + $(this).attr('href') + '"></iframe>');
-        
-        })
-        
-    });
-</script>
