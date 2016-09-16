43237014584343a155ebf2e11e856a50be9d2825
Henry Garcia <hjgarcia1@gmail.com>
2016-09-16 10:48:46 -0400

    added meta tags and ico file

--- resources/views/layouts/partials/meta.blade.php
+++ resources/views/layouts/partials/meta.blade.php
@@ -1,4 +1,6 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="canonical" href="http://{{ URL::current() }}">
+<meta name="description" content="IAT provides the resources and technology to support the implementation of successful Science, Technology, Engineering, and Mathematics (STEM) curricula.">
 <title>Next Gen PET {{ (isset($pageTitle)) ? ' | ' . $pageTitle :'' }}</title>
--- resources/views/layouts/partials/styles.blade.php
+++ resources/views/layouts/partials/styles.blade.php
@@ -1,3 +1,4 @@
+<link rel="shortcut icon" href="{{ asset('img/favicon.png') }}">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,700,700i|Rasa:300,400,500,700"
       rel="stylesheet">
 
