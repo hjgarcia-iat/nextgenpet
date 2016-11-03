13060723169619180c53f9335c66f466f2f33a06
Henry Garcia <hjgarcia1@gmail.com>
2016-11-03 10:33:38 -0400

    added new sidebar links

--- resources/views/pages/lecture-style-class/_sidebar.blade.php
+++ resources/views/pages/lecture-style-class/_sidebar.blade.php
@@ -36,11 +36,34 @@
                {{--href="{{ url('lecture-style-class/content/matter-and-interactions-module') }}">Matter and Interactions Module--}}
             {{--</a>--}}
         {{--</div>--}}
+    
+        <a href="#" class="list-group-item sublink collapsed" data-toggle="collapse"
+        data-target="#sm"
+        data-parent="#menu">Student Materials (for viewing only)</a>
+        <div id="sm" class="sublinks collapse">
+            <a class="list-group-item"
+               target="_blank" data-file="true"
+            href="{{ asset('docs/lecture/Module_MSE_Lecture-Style.pdf') }}">Magnetism and Static Electricity Module
+            </a>
+            <a class="list-group-item"
+               target="_blank" data-file="true"
+            href="{{ asset('docs/lecture/Module_IE_Lecture-Style.pdf') }}">Interactions and Energy Module
+            </a>
+            <a class="list-group-item"
+               target="_blank" data-file="true"
+            href="{{ asset('docs/lecture/Module_IF_Lecture-Style.pdf') }}">Interactions and Forces Module
+            </a>
+            <a class="list-group-item"
+               target="_blank" data-file="true"
+            href="{{ asset('docs/lecture/Module_WSL_Lecture-Style.pdf') }}">Waves, Sounds and Light Module
+            </a>
+            <a class="list-group-item"
+               target="_blank" data-file="true"
+            href="{{ asset('docs/lecture/Module_MI_Lecture-Style.pdf') }}">Matter and Interactions Module
+            </a>
+        </div>
         
-        <a href="{{ asset('docs/Next_Gen_PET_Lecture-Style.pdf') }}"
-           data-file="true" target="_blank"
-           class="list-group-item">Student Materials (for viewing only)
-        </a>
+        
         <a href="{{ asset('docs/Teaching_&_Learning_Lecture_Style.pdf') }}"
            target="_blank" data-file="true"
            class="list-group-item">Teaching and Learning Materials
--- resources/views/pages/studio-style-class/_sidebar.blade.php
+++ resources/views/pages/studio-style-class/_sidebar.blade.php
@@ -35,11 +35,33 @@
                {{--href="{{ url('studio-style-class/content/matter-and-interactions-module') }}">Matter and Interactions Module--}}
             {{--</a>--}}
         {{--</div>--}}
-        
-        <a href="{{ asset('docs/Next_Gen_PET_Studio-style_Class.pdf') }}"
-           data-file="true" target="_blank"
-           class="list-group-item">Student Materials (for viewing only)
+    
+        <a href="#" class="list-group-item sublink collapsed" data-toggle="collapse"
+           data-target="#sm"
+           data-parent="#menu">Student Materials (for viewing only)
         </a>
+        <div id="sm" class="sublinks collapse">
+            <a class="list-group-item"
+               target="_blank" data-file="true"
+               href="{{ asset('docs/studio/Module_MSE_Studio_Style.pdf') }}">Magnetism and Static Electricity Module
+            </a>
+            <a class="list-group-item"
+               target="_blank" data-file="true"
+               href="{{ asset('docs/studio/Module_IE_Studio_Style.pdf') }}">Interactions and Energy Module
+            </a>
+            <a class="list-group-item"
+               target="_blank" data-file="true"
+               href="{{ asset('docs/studio/Module_IF_Studio_Style.pdf') }}">Interactions and Forces Module
+            </a>
+            <a class="list-group-item"
+               target="_blank" data-file="true"
+               href="{{ asset('docs/studio/Module_WSL_Studio_Style.pdf') }}">Waves, Sounds and Light Module
+            </a>
+            <a class="list-group-item"
+               target="_blank" data-file="true"
+               href="{{ asset('docs/studio/Module_MI_Studio_Style.pdf') }}">Matter and Interactions Module
+            </a>
+        </div>
         <a href="{{ asset('docs/Teaching_&_Learning_Studio_Style.pdf') }}"
            target="_blank" data-file="true"
            class="list-group-item">Teaching and Learning Materials
