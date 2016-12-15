85b606be242d8d97e72de35d0ea6458fed34301d
Henry Garcia <hjgarcia1@gmail.com>
2016-12-15 09:56:53 -0500

    more footer changes

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,25 +1,31 @@
-<section class="page-footer">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="page-footer-content">
-                    <p class="text-muted text-center">
-                    <sup>&copy;</sup> It's About Time {{ date('Y') }}. All Rights Reserved.
-                    </p>
-                </div>
-                <div class="page-footer-logos">
-                    <div class="row row-eq-height">
-                        <div class="col-md-3">&nbsp;</div>
-                        <div class="col-md-2"><img src="{{ asset('img/nsf.png') }}" class="center-block img-responsive" alt="National Science Foundation" width="95"></div>
-                        <div class="col-md-2"><img src="{{ asset('img/chevron.png') }}" class="center-block img-responsive" alt=""
-                                                   width="95"></div>
-                        <div class="col-md-2"><img src="{{ asset('img/betchel.png') }}" class="center-block img-responsive" alt=""
-                                                   width="150"></div>
-                        <div class="col-md-3">&nbsp;</div>
+<div class="page-footer-wrapper">
+    <section class="page-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-footer-content">
+                        <p class="text-muted text-center">
+                        <sup>&copy;</sup> It's About Time {{ date('Y') }}. All Rights Reserved.
+                        </p>
+                    </div>
+                    <div class="page-footer-logos">
+                        <div class="row row-eq-height">
+                            <div class="col-md-3">&nbsp;</div>
+                            <div class="col-md-2"><img src="{{ asset('img/nsf.png') }}"
+                                                       class="center-block img-responsive"
+                                                       alt="National Science Foundation" width="95"></div>
+                            <div class="col-md-2"><img src="{{ asset('img/chevron.png') }}"
+                                                       class="center-block img-responsive" alt=""
+                                                       width="95"></div>
+                            <div class="col-md-2"><img src="{{ asset('img/betchel.png') }}"
+                                                       class="center-block img-responsive" alt=""
+                                                       width="150"></div>
+                            <div class="col-md-3">&nbsp;</div>
+                        </div>
                     </div>
                 </div>
-            </div>
             
+            </div>
         </div>
-    </div>
-</section>
+    </section>
+</div>
