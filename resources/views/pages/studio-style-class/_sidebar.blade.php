fafd9aa2a1c0080fade1fba652a1d9b34b316260
Henry Garcia <hjgarcia1@gmail.com>
2016-12-22 09:33:48 -0500

    opened up content modules page again

--- resources/views/pages/studio-style-class/_sidebar.blade.php
+++ resources/views/pages/studio-style-class/_sidebar.blade.php
@@ -14,27 +14,27 @@
            class="list-group-item{{ (Request::is('studio-style-class/table-of-contents')) ? ' active' : '' }}">Table of Contents with Time Estimates
         </a>
         
-        {{--<a href="#" class="list-group-item sublink collapsed" data-toggle="collapse"--}}
-           {{--data-target="#sm"--}}
-           {{--data-parent="#menu">Content Modules--}}
-        {{--</a>--}}
-        {{--<div id="sm" class="sublinks collapse{{ (Request::is('studio-style-class/content/*') ? ' in' : '') }}">--}}
-            {{--<a class="list-group-item {{ (Request::is('studio-style-class/content/magnetism-and-static-electricity-module') ? ' active' : '') }}"--}}
-               {{--href="{{ url('studio-style-class/content/magnetism-and-static-electricity-module') }}">Magnetism and Static Electricity Module--}}
-            {{--</a>--}}
-            {{--<a class="list-group-item{{ (Request::is('studio-style-class/content/interactions-and-energy-module') ? ' active' : '') }}"--}}
-               {{--href="{{ url('studio-style-class/content/interactions-and-energy-module') }}">Interactions and Energy Module--}}
-            {{--</a>--}}
-            {{--<a class="list-group-item{{ (Request::is('studio-style-class/content/interactions-and-forces-module') ? ' active' : '') }}"--}}
-               {{--href="{{ url('studio-style-class/content/interactions-and-forces-module') }}">Interactions and Forces Module--}}
-            {{--</a>--}}
-            {{--<a class="list-group-item{{ (Request::is('studio-style-class/content/waves-sound-and-light-module') ? ' active' : '') }}"--}}
-               {{--href="{{ url('studio-style-class/content/waves-sound-and-light-module') }}">Waves, Sounds and Light Module--}}
-            {{--</a>--}}
-            {{--<a class="list-group-item{{ (Request::is('studio-style-class/content/matter-and-interactions-module') ? ' active' : '') }}"--}}
-               {{--href="{{ url('studio-style-class/content/matter-and-interactions-module') }}">Matter and Interactions Module--}}
-            {{--</a>--}}
-        {{--</div>--}}
+        <a href="#" class="list-group-item sublink collapsed" data-toggle="collapse"
+           data-target="#sm"
+           data-parent="#menu">Content Modules
+        </a>
+        <div id="sm" class="sublinks collapse{{ (Request::is('studio-style-class/content/*') ? ' in' : '') }}">
+            <a class="list-group-item {{ (Request::is('studio-style-class/content/magnetism-and-static-electricity-module') ? ' active' : '') }}"
+               href="{{ url('studio-style-class/content/magnetism-and-static-electricity-module') }}">Magnetism and Static Electricity Module
+            </a>
+            <a class="list-group-item{{ (Request::is('studio-style-class/content/interactions-and-energy-module') ? ' active' : '') }}"
+               href="{{ url('studio-style-class/content/interactions-and-energy-module') }}">Interactions and Energy Module
+            </a>
+            <a class="list-group-item{{ (Request::is('studio-style-class/content/interactions-and-forces-module') ? ' active' : '') }}"
+               href="{{ url('studio-style-class/content/interactions-and-forces-module') }}">Interactions and Forces Module
+            </a>
+            <a class="list-group-item{{ (Request::is('studio-style-class/content/waves-sound-and-light-module') ? ' active' : '') }}"
+               href="{{ url('studio-style-class/content/waves-sound-and-light-module') }}">Waves, Sounds and Light Module
+            </a>
+            <a class="list-group-item{{ (Request::is('studio-style-class/content/matter-and-interactions-module') ? ' active' : '') }}"
+               href="{{ url('studio-style-class/content/matter-and-interactions-module') }}">Matter and Interactions Module
+            </a>
+        </div>
     
         <a href="#" class="list-group-item sublink collapsed" data-toggle="collapse"
            data-target="#sm-student-materials"
