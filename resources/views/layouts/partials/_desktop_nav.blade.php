41e607270731535cce6ae22da49cae96fc35caf8
Henry Garcia <hjgarcia1@gmail.com>
2017-02-08 11:23:57 -0500

    navbar fixes

--- resources/views/layouts/partials/_desktop_nav.blade.php
+++ resources/views/layouts/partials/_desktop_nav.blade.php
@@ -5,13 +5,13 @@
                 <a class="nav-link" href="{{ url('/') }}">Home</a>
             </li>
     
-            <li class="nav-item {{ Request::is('studio-style-class') ? 'active' : '' }}">
+            <li class="nav-item {{ (Request::is('studio-style-class') or Request::is('studio-style-class/*')) ? 'active' : '' }}">
                 <a class="nav-link"
                    href="{{ url('studio-style-class') }}">Studio Style Class
                 </a>
             </li>
             
-            <li class="nav-item {{ (Request::is('lecture-style-class')) ? 'active' : '' }}">
+            <li class="nav-item {{ (Request::is('lecture-style-class') or Request::is('lecture-style-class/*')) ? 'active' : '' }}">
                 <a class="nav-link"
                    href="{{ url('lecture-style-class') }}">Lecture Style Class
                 </a>
