f905a70f592d4b95dba2ccf4473642783c4111e4
Henry Garcia <hjgarcia1@gmail.com>
2017-02-08 11:04:59 -0500

    pdfs now loading inline

---
+++ resources/assets/js/app.js
@@ -0,0 +1,16 @@
+$(function () {
+    $('.page-side-menu a[data-file="true"]').on('click', function (e) {
+        e.preventDefault();
+
+        var index = $(this).index();
+
+        $('.page-side-menu a.active').removeClass('active');
+        $(this).siblings('a').removeClass('active');
+
+        $(this).addClass('active');
+
+        $('#html-content').hide().siblings('#pdf-content').show();
+        $('#pdf-content').html('<h3>' + $(this).text() + '</h3><iframe class="iframe" src="' + $(this).attr('href') + '"></iframe>');
+
+    });
+});
