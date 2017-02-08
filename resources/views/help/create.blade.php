c42b0d7f21cd8688c74673315e191daba9312dd6
Henry Garcia <hjgarcia1@gmail.com>
2017-02-08 10:43:47 -0500

    help area fixes for bootstrap 4

--- resources/views/help/create.blade.php
+++ resources/views/help/create.blade.php
@@ -5,23 +5,29 @@
         @include('layouts.partials.styles')
     </head>
     <body>
-        @include('layouts.partials.mobile_nav')
         @include('layouts.partials.header')
         
         @include('layouts.partials.title')
         <section class="banner-image visible-md visible-lg">
-            <div class="fill" style="background-image:url('../img/lecture-style-class.jpg');">
+            <div class="fill"
+                 style="background-image:url('../img/lecture-style-class.jpg');">
                 <div class="container">
                 
                 </div>
             </div>
         </section>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h2>Questions? Comments?</h2>
+        
+        <section class="page-title">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2>Questions? Comments?</h2>
+                    </div>
                 </div>
             </div>
+        </section>
+        
+        <div class="container">
             <div class="row">
                 <div class="col-md-12">
                     
@@ -33,45 +39,59 @@
                     
                     {{ Form::open(['class' => 'form']) }}
                     
-                    <div class="form-group{{ ($errors->first('name') ? ' has-error' : '') }}">
-                        <label class="control-label" for="name">Name</label> <input type="text" class="form-control"
-                                                                                    id="name"
-                                                                                    placeholder="Name" name="name"
-                                                                                    value="{{ old('name') }}">
-                        {!! $errors->first('name','<span class="help-block">:message</span>') !!}
+                    <div class="form-group{{ ($errors->first('name') ? ' has-danger' : '') }}">
+                        <label class="form-control-label"
+                               for="name">Name
+                        </label>
+                        <input type="text"
+                               class="form-control{{ ($errors->first('name') ? ' form-control-danger' : '') }}"
+                               id="name"
+                               name="name"
+                               value="{{ old('name') }}">
+                        {!! $errors->first('name','<div class="form-control-feedback">:message</div>') !!}
                     </div>
                     
-                    <div class="form-group{{ ($errors->first('email') ? ' has-error' : '') }}">
-                        <label class="control-label" for="email">Email Address</label> <input type="email"
-                                                                                              class="form-control"
-                                                                                              id="email"
-                                                                                              placeholder="Email"
-                                                                                              name="email"
-                                                                                              value="{{ old('email') }}">
-                        {!! $errors->first('name','<span class="help-block">:message</span>') !!}
+                    <div class="form-group{{ ($errors->first('email') ? ' has-danger' : '') }}">
+                        <label class="form-control-label"
+                               for="email">Email Address
+                        </label>
+                        <input type="email"
+                               class="form-control{{ ($errors->first('email') ? ' form-control-danger' : '') }}"
+                               id="email"
+                               name="email"
+                               value="{{ old('email') }}">
+                        {!! $errors->first('name','<div class="form-control-feedback">:message</div>') !!}
                     </div>
                     
-                    <div class="form-group{{ ($errors->first('subject') ? ' has-error' : '') }}">
-                        <label class="control-label" for="subject">Subject</label> <input type="text"
-                                                                                          class="form-control"
-                                                                                          id="subject"
-                                                                                          placeholder="Subject"
-                                                                                          name="subject"
-                                                                                          value="{{ old('subject') }}">
-                        {!! $errors->first('subject','<span class="help-block">:message</span>') !!}
+                    <div class="form-group{{ ($errors->first('subject') ? ' has-danger' : '') }}">
+                        <label class="form-control-label"
+                               for="subject">Subject
+                        </label>
+                        <input type="text"
+                               class="form-control{{ ($errors->first('subject') ? ' form-control-danger' : '') }}"
+                               id="subject"
+                               name="subject"
+                               value="{{ old('subject') }}">
+                        {!! $errors->first('subject','<div class="form-control-feedback">:message</div>') !!}
                     </div>
                     
-                    <div class="form-group{{ ($errors->first('comment') ? ' has-error' : '') }}">
-                        <label class="control-label" for="comment">Message</label> <textarea name="comment" id="comment"
-                                                                                             class="form-control"
-                                                                                             placeholder="Message"
-                                                                                             cols="30"
-                                                                                             rows="10"></textarea>
+                    <div class="form-group{{ ($errors->first('comment') ? ' has-danger' : '') }}">
+                        <label class="form-control-label"
+                               for="comment">Message
+                        </label>
+                        <textarea name="comment"
+                                  id="comment"
+                                  class="form-control{{ ($errors->first('comment') ? ' form-control-danger' : '') }}"
+                                  cols="30"
+                                  rows="10"></textarea>
                         
-                        {!! $errors->first('comment','<span class="help-block">:message</span>') !!}
+                        {!! $errors->first('comment','<div class="form-control-feedback">:message</div>') !!}
                     </div>
                     
-                    <input type="submit" name="submit" class="btn btn-primary" value="Submit">
+                    <input type="submit"
+                           name="submit"
+                           class="btn btn-primary"
+                           value="Submit">
                     
                     {{ Form::close() }}
                 </div>
@@ -83,14 +103,25 @@
                 <div class="col-xs-12 col-md-5">
                     <h4>Contact Us</h4>
                     <p>
-                    It's About Time<sup>&reg;</sup> <br> 333 North Bedford Road Suite 110 <br> Mount Kisco, N.Y. 10549
+                        It's About Time
+                        <sup>&reg;</sup>
+                        <br>
+                        333 North Bedford Road Suite 110
+                        <br>
+                        Mount Kisco, N.Y. 10549
                     </p>
                     <p>
-                    Toll Free: 1-888-698-TIME <br> Office: 914-273-2233 <br> Fax: 914-206-6444
+                        Toll Free: 1-888-698-TIME
+                        <br>
+                        Office: 914-273-2233
+                        <br>
+                        Fax: 914-206-6444
                     </p>
                 </div>
                 <div class="col-md-7 visible-md visible-lg">
-                    <img src="{{ asset('img/contact-us.jpg') }}" class="img-responsive img-thumbnail" alt="">
+                    <img src="{{ asset('img/contact-us.jpg') }}"
+                         class="img-responsive img-thumbnail"
+                         alt="">
                 </div>
             </div>
         
