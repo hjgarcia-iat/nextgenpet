f276a5d5bbae5b733518e1032d03018e6a135629
Henry Garcia <hjgarcia1@gmail.com>
2017-02-15 09:38:59 -0500

    added new  login link to navs

--- resources/views/layouts/partials/_desktop_nav.blade.php
+++ resources/views/layouts/partials/_desktop_nav.blade.php
@@ -20,16 +20,11 @@
             <li class="nav-item {{ (Request::is('help')) ? 'active' : '' }}">
                 <a class="nav-link" href="{{ url('help') }}">Help</a>
             </li>
-        
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="loginlinks"
-                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Additional Instructor Resources
+    
+            <li class="nav-item {{ (Request::is('login')) ? 'active' : '' }}">
+                <a class="nav-link"
+                   href="{{ url('login') }}">Login
                 </a>
-                <div class="dropdown-menu" aria-labelledby="loginlinks">
-                    <a class="dropdown-item" href="https://petcyberpd.iat.com/unit/studio-style/" target="_blank">Studio Style</a>
-                    <a class="dropdown-item" href="https://petcyberpd.iat.com/unit/lecture-style/" target="_blank">Lecture Style</a>
-                </div>
             </li>
         </ul>
     </div>
--- resources/views/layouts/partials/_mobile_nav.blade.php
+++ resources/views/layouts/partials/_mobile_nav.blade.php
@@ -327,26 +327,10 @@
                 </a>
             </li>
     
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle"
-                   href="#"
-                   id="loginlinks"
-                   data-toggle="dropdown"
-                   aria-haspopup="true"
-                   aria-expanded="false">
-                    Additional Instructor Resources
+            <li class="nav-item {{ (Request::is('login')) ? 'active' : '' }}">
+                <a class="nav-link"
+                   href="{{ url('login') }}">Login
                 </a>
-                <div class="dropdown-menu"
-                     aria-labelledby="loginlinks">
-                    <a class="dropdown-item"
-                       href="https://petcyberpd.iat.com/unit/studio-style/"
-                       target="_blank">Studio Style
-                    </a>
-                    <a class="dropdown-item"
-                       href="https://petcyberpd.iat.com/unit/lecture-style/"
-                       target="_blank">Lecture Style
-                    </a>
-                </div>
             </li>
 
         </ul>
