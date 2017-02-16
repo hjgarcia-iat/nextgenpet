772afa7077c4a6389057cceec9fb71579abf6666
Henry Garcia <hjgarcia1@gmail.com>
2017-02-16 14:45:01 -0500

    mobile fixes

--- resources/views/layouts/partials/title.blade.php
+++ resources/views/layouts/partials/title.blade.php
@@ -1,4 +1,4 @@
-<section class="title-bar pb-2 pt-2">
+<section class="title-bar pb-2 pt-2 hidden-sm-down">
     <div class="container">
         <h1 class="text-center">Next GEN Physical Science and Everyday Thinking</h1>
     </div>
