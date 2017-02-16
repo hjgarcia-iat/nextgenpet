f8347e080cb64166fb9332568faa3288fa5f10a8
Henry Garcia <hjgarcia1@gmail.com>
2017-02-16 15:50:30 -0500

    smaller canvas on mobile

--- resources/views/layouts/partials/meta.blade.php
+++ resources/views/layouts/partials/meta.blade.php
@@ -1,6 +1,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport"
+      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 <link rel="canonical" href="http://{{ URL::current() }}">
 <meta name="description" content="IAT provides the resources and technology to support the implementation of successful Science, Technology, Engineering, and Mathematics (STEM) curricula.">
 <title>Next Gen PET | @yield('pageTitle')</title>
