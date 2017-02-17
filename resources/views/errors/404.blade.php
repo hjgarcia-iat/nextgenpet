98fba04ee9ecaadd479027c6606a550b37e0e368
Henry Garcia <hjgarcia1@gmail.com>
2017-02-17 10:34:03 -0500

    error page fixes

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,25 +5,32 @@
         @include('layouts.partials.styles')
     </head>
     <body>
-        @include('layouts.partials.header')
-        <div class="container" style="height: 100%;">
-            <div class="row justify-content-center"
-                 style="height: 100%">
-                <div class="col-md-8" style="align-self: center">
-                    <div class="well well-error">
-                        <h2 class="text-center">Oops!</h2>
-                        <p class="lead text-center">We can't seem to find the page you're looking for... <br><span class="text-muted"><small>Error Code: 404</small></span></p>
-
-                        <p class="text-center">
-                        <a class="btn btn-primary btn-lg" href="/">Back to Home Page</a>
-                        </p>
-
-                    </div>
+        
+        <div class="page-wrapper">
+            <div class="site-header-wrapper">
+                @include('layouts.partials.header')
+            </div>
+    
+            <div class="page-content-wrapper justify-content-center align-items-center">
+                <div class="well well-error text-center mx-auto w-25">
+                    <h3>We can't seem to find the page you're looking for...</h3>
+                    <p>
+                        <span class="text-muted">
+                            Error Code: 404
+                        </span>
+                    </p>
+        
+                    <p>
+                        <a class="btn btn-primary"
+                           href="/">Back to Home Page
+                        </a>
+                    </p>
                 </div>
             </div>
+            <div class="page-footer-wrapper">
+                @include('layouts.partials.footer')
+            </div>
         </div>
-
-        @include('layouts.partials.footer')
         @include('layouts.partials.scripts')
     </body>
 </html>
--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -7,64 +7,7 @@
     <body>
         <div class="page-wrapper">
             <div class="site-header-wrapper">
-                <header class="site-header">
-                    <section class="top-row">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <section class="logo text-sm-center text-xs-center">
-                                        <a href="/"
-                                           class="full mx-auto"></a>
-                                    </section>
-                                </div>
-                                <div class="col-md-8">
-                                    <div class="row justify-content-md-end">
-                                        <div class="col-sm-12 col-md-7 col-lg-5">
-                                            <section class="social-bar d-xs-flex d-sm-flex justify-content-xs-between justify-content-sm-between">
-                                                <div>
-                                                    <a href="http://blog.iat.com"
-                                                       target="_blank">
-                                                        <i class="fa fa-comment"></i>
-                                                    </a>
-                                                </div>
-                                                <div>
-                                                    <a href="https://www.facebook.com/IATlearning"
-                                                       target="_blank">
-                                                        <i class="fa fa-facebook"></i>
-                                                    </a>
-                                                </div>
-                                                <div>
-                                                    <a href="https://twitter.com/ItsAboutTimeEdu"
-                                                       target="_blank">
-                                                        <i class="fa fa-twitter"></i>
-                                                    </a>
-                                                </div>
-                                                <div>
-                                                    <a href="http://www.linkedin.com/company/it's-about-time-learning"
-                                                       target="_blank">
-                                                        <i class="fa fa-linkedin"></i>
-                                                    </a>
-                                                </div>
-                                                <div>
-                                                    <a href="http://www.youtube.com/user/itsaboutimeedu"
-                                                       target="_blank">
-                                                        <i class="fa fa-youtube"></i>
-                                                    </a>
-                                                </div>
-                                                <div>
-                                                    <a href="http://instagram.com/itsabouttimeedu"
-                                                       target="_blank">
-                                                        <i class="fa fa-instagram"></i>
-                                                    </a>
-                                                </div>
-                                            </section>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                </header>
+                @include('layouts.partials._logo_area')
             </div>
     
             <div class="page-content-wrapper justify-content-center align-items-center">
