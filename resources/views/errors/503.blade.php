0755a00dbcac499d1f48ab11f3495d38342c2b9b
Henry Garcia <hjgarcia1@gmail.com>
2017-02-17 10:22:43 -0500

    503 page fixed

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -5,79 +5,84 @@
         @include('layouts.partials.styles')
     </head>
     <body>
-    
-        <header class="site-header">
-            <section class="top-row">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <section class="logo text-sm-center text-xs-center">
-                                <a href="/"
-                                   class="full mx-auto"></a>
-                            </section>
-                        </div>
-                        <div class="col-md-8">
-                            <div class="row justify-content-md-end">
-                                <div class="col-sm-12 col-md-7 col-lg-5">
-                                    <section class="social-bar d-xs-flex d-sm-flex justify-content-xs-between justify-content-sm-between">
-                                        <div>
-                                            <a href="http://blog.iat.com"
-                                               target="_blank">
-                                                <i class="fa fa-comment"></i>
-                                            </a>
-                                        </div>
-                                        <div>
-                                            <a href="https://www.facebook.com/IATlearning"
-                                               target="_blank">
-                                                <i class="fa fa-facebook"></i>
-                                            </a>
-                                        </div>
-                                        <div>
-                                            <a href="https://twitter.com/ItsAboutTimeEdu"
-                                               target="_blank">
-                                                <i class="fa fa-twitter"></i>
-                                            </a>
-                                        </div>
-                                        <div>
-                                            <a href="http://www.linkedin.com/company/it's-about-time-learning"
-                                               target="_blank">
-                                                <i class="fa fa-linkedin"></i>
-                                            </a>
-                                        </div>
-                                        <div>
-                                            <a href="http://www.youtube.com/user/itsaboutimeedu"
-                                               target="_blank">
-                                                <i class="fa fa-youtube"></i>
-                                            </a>
-                                        </div>
-                                        <div>
-                                            <a href="http://instagram.com/itsabouttimeedu"
-                                               target="_blank">
-                                                <i class="fa fa-instagram"></i>
-                                            </a>
-                                        </div>
+        <div class="page-wrapper">
+            <div class="site-header-wrapper">
+                <header class="site-header">
+                    <section class="top-row">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <section class="logo text-sm-center text-xs-center">
+                                        <a href="/"
+                                           class="full mx-auto"></a>
                                     </section>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="row justify-content-md-end">
+                                        <div class="col-sm-12 col-md-7 col-lg-5">
+                                            <section class="social-bar d-xs-flex d-sm-flex justify-content-xs-between justify-content-sm-between">
+                                                <div>
+                                                    <a href="http://blog.iat.com"
+                                                       target="_blank">
+                                                        <i class="fa fa-comment"></i>
+                                                    </a>
+                                                </div>
+                                                <div>
+                                                    <a href="https://www.facebook.com/IATlearning"
+                                                       target="_blank">
+                                                        <i class="fa fa-facebook"></i>
+                                                    </a>
+                                                </div>
+                                                <div>
+                                                    <a href="https://twitter.com/ItsAboutTimeEdu"
+                                                       target="_blank">
+                                                        <i class="fa fa-twitter"></i>
+                                                    </a>
+                                                </div>
+                                                <div>
+                                                    <a href="http://www.linkedin.com/company/it's-about-time-learning"
+                                                       target="_blank">
+                                                        <i class="fa fa-linkedin"></i>
+                                                    </a>
+                                                </div>
+                                                <div>
+                                                    <a href="http://www.youtube.com/user/itsaboutimeedu"
+                                                       target="_blank">
+                                                        <i class="fa fa-youtube"></i>
+                                                    </a>
+                                                </div>
+                                                <div>
+                                                    <a href="http://instagram.com/itsabouttimeedu"
+                                                       target="_blank">
+                                                        <i class="fa fa-instagram"></i>
+                                                    </a>
+                                                </div>
+                                            </section>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </section>
-        </header>
-        
-        
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
+                    </section>
+                </header>
+            </div>
+    
+            <div class="page-content-wrapper justify-content-center align-items-center">
+    
+                <section class="error">
                     <div class="well well-error">
                         <h2 class="text-center">Be right back!</h2>
                         <p class="lead text-center">We are making changes to the website</p>
                     </div>
-                </div>
+                </section>
+
             </div>
+            
+            <div class="page-footer-wrapper">
+                @include('layouts.partials.footer')
+            </div>
+            
+            @include('layouts.partials.scripts')
         </div>
-        
-        @include('layouts.partials.footer')
-        @include('layouts.partials.scripts')
     </body>
 </html>
--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,18 +1,18 @@
-<div class="page-footer-wrapper">
-    <section class="page-footer">
-        <div class="container">
-            <div class="page-footer-content">
-                <div class="row">
-                    <div class="col-md-12">
-                        <p class="text-muted text-center">
-                            <sup>&copy;</sup>
-                            It's About Time {{ date('Y') }}. All Rights Reserved.
-                        </p>
-                    </div>
+
+
+<section class="page-footer">
+    <div class="container">
+        <div class="page-footer-content">
+            <div class="row">
+                <div class="col-md-12">
+                    <p class="text-muted text-center">
+                        <sup>&copy;</sup>
+                        It's About Time {{ date('Y') }}. All Rights Reserved.
+                    </p>
                 </div>
             </div>
-            
-            
         </div>
-    </section>
-</div>
+    
+    
+    </div>
+</section>
