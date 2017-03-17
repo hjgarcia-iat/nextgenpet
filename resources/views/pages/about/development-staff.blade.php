052090c241b2cc2990579145a71be772d2794db4
Henry Garcia <hjgarcia1@gmail.com>
2017-03-17 13:27:19 -0400

    home area fixes

--- resources/views/pages/about/development-staff.blade.php
+++ resources/views/pages/about/development-staff.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.two-column')
 
-@section('pageTitle')
-    About The Development Staff
-@stop
+@section('pageTitle','About The Development Staff')
+
 @section('sidebar')
     @include('home._sidebar')
 @stop
@@ -12,60 +11,103 @@
 
 @section('content')
     
+    <h3>Development Staff</h3>
+    <h4>Next Gen PET developers</h4>
+    <p>
+        Fred Goldberg, San Diego State University
+        <br>
+        Steve Robinson, Tennessee Technological University
+        <br>
+        Ed Price, California State University San Marcos
+        <br>
+        Danielle Harlow, University of California, Santa Barbara
+        <br>
+        Julie Andrew, University of Colorado at Boulder
+        <br>
+        Michael McKean, San Diego State University
+    </p>
     
-    <section class="html-content">
-        <h3>Development Staff</h3>
-        <h4>Next Gen PET developers</h4>
-        <p>
-        Fred Goldberg, San Diego State University <br> Steve Robinson, Tennessee Technological University
-        <br> Ed Price, California State University San Marcos
-        <br> Danielle Harlow, University of California, Santa Barbara
-        <br> Julie Andrew, University of Colorado at Boulder <br> Michael McKean, San Diego State University
-        </p>
-        
-        
-        <h4>Next Gen PET contributors and/or field tester</h4>
-        <p>
-        Valerie Otero, University of Colorado at Boulder <br>Rebecca Stober, University of Colorado at Boulder
-        <br>Cary Sneider, Portland State University <br>Leslie Atkins, Boise State University
-        <br>Paula Engelhardt, Tennessee Technological University
-        <br>David Mitchell, California Polytechnic State University at San Luis Obispo
-        <br>Anne Marie Bergen, California Polytechnic State University at San Luis Obispo
-        <br>Nancy Stauch, California Polytechnic State University at San Luis Obispo
-        <br>Lola Berber-Jimenez, California Polytechnic State University at San Luis Obispo
-        <br>Tina Duran, California Polytechnic State University at San Luis Obispo
-        </p>
-        
-        <h4>Videographers, Editors and Transcribers</h4>
-        <p>
-        Megan Santos, San Diego State University <br>Katie Badham, San Diego State University
-        <br>James Powell, San Diego State University <br>Shawn Alff, San Diego State University
-        <br>Janeth Cruz, San Diego State University <br>Erika Guzman, San Diego State University
-        <br>Leo Farias, California Polytechnic State University at San Luis Obispo
-        <br>Loren Edwards, California Polytechnic State University at San Luis Obispo
-        <br>Kristin Bridgeford, California Polytechnic State University at San Luis Obispo
-        <br>Sarah Hegg, California Polytechnic State University at San Luis Obispo
-        <br>Ryan Calloway, Tennessee Technological University <br>Carla Moore, Tennessee Technological University
-        <br>Ian Robinson, Tennessee Technological University
-        </p>
-        
-        <h4>Technical Assistance</h4>
-        <p>
+    
+    <h4>Next Gen PET contributors and/or field tester</h4>
+    <p>
+        Valerie Otero, University of Colorado at Boulder
+        <br>
+        Rebecca Stober, University of Colorado at Boulder
+        <br>
+        Cary Sneider, Portland State University
+        <br>
+        Leslie Atkins, Boise State University
+        <br>
+        Paula Engelhardt, Tennessee Technological University
+        <br>
+        David Mitchell, California Polytechnic State University at San Luis Obispo
+        <br>
+        Anne Marie Bergen, California Polytechnic State University at San Luis Obispo
+        <br>
+        Nancy Stauch, California Polytechnic State University at San Luis Obispo
+        <br>
+        Lola Berber-Jimenez, California Polytechnic State University at San Luis Obispo
+        <br>
+        Tina Duran, California Polytechnic State University at San Luis Obispo
+    </p>
+    
+    <h4>Videographers, Editors and Transcribers</h4>
+    <p>
+        Megan Santos, San Diego State University
+        <br>
+        Katie Badham, San Diego State University
+        <br>
+        James Powell, San Diego State University
+        <br>
+        Shawn Alff, San Diego State University
+        <br>
+        Janeth Cruz, San Diego State University
+        <br>
+        Erika Guzman, San Diego State University
+        <br>
+        Leo Farias, California Polytechnic State University at San Luis Obispo
+        <br>
+        Loren Edwards, California Polytechnic State University at San Luis Obispo
+        <br>
+        Kristin Bridgeford, California Polytechnic State University at San Luis Obispo
+        <br>
+        Sarah Hegg, California Polytechnic State University at San Luis Obispo
+        <br>
+        Ryan Calloway, Tennessee Technological University
+        <br>
+        Carla Moore, Tennessee Technological University
+        <br>
+        Ian Robinson, Tennessee Technological University
+    </p>
+    
+    <h4>Technical Assistance</h4>
+    <p>
         Lauran Gerhart, California State University at San Marcos
-        <br>Nathaniel Reynolds, California State University at San Marcos The California State University at San Marcos Instructional and Information Technology Services
-        </p>
-        <h4>Developers of previous versions</h4>
-        <p>
-        <em>Next Gen PET</em> was adapted from four previous curricula: <em>Physics and Everyday Thinking (PET)</em>,
-        <em>Physical Science and Everyday Thinking (PSET)</em>, <em>Learning Physical Science (LEPS)</em> and
-        <em>Learning Physics (LEP)</em>. Several of the activities in
-        <em>Next Gen PET</em> are very similar to the activities included in the previous curricula. The co-authors of PET, PSET, LEPS and/or LEP who are not currently working on Next Gen PET are listed below since their contributions are reflected in the revised materials.
-        </p>
-        <p>
-        Valerie Otero, University of Colorado, Boulder <br>Rebecca Kruse, National Science Foundation
-        <br>Nephi Thompson, Colorado Mountain College
-        </p>
-    
-    </section>
+        <br>
+        Nathaniel Reynolds, California State University at San Marcos The California State University at San Marcos Instructional and Information Technology Services
+    </p>
+    <h4>Developers of previous versions</h4>
+    <p>
+        <em>Next Gen PET</em>
+        was adapted from four previous curricula:
+        <em>Physics and Everyday Thinking (PET)</em>
+        ,
+        <em>Physical Science and Everyday Thinking (PSET)</em>
+        ,
+        <em>Learning Physical Science (LEPS)</em>
+        and
+        <em>Learning Physics (LEP)</em>
+        . Several of the activities in
+        <em>Next Gen PET</em>
+        are very similar to the activities included in the previous curricula. The co-authors of PET, PSET, LEPS and/or LEP who are not currently working on Next Gen PET are listed below since their contributions are reflected in the revised materials.
+    </p>
+    <p>
+        Valerie Otero, University of Colorado, Boulder
+        <br>
+        Rebecca Kruse, National Science Foundation
+        <br>
+        Nephi Thompson, Colorado Mountain College
+    </p>
+
 
 @stop
--- resources/views/pages/about/research-based.blade.php
+++ resources/views/pages/about/research-based.blade.php
@@ -11,63 +11,74 @@
 
 @section('content')
     
-    <section class="html-content">
-        
-        <p>
+    
+    
+    <p>
         The development of
-        <em>Next Gen PET</em> and its predecessor curricula was guided by five design principles based on research on learning. The following table shows how the design principles are put into action in
-        <em>Next Gen PET</em>.
-        </p>
-        <div class="table-responsive">
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>Design Principles</th>
-                        <th>Studio-style class (SC)</th>
-                        <th>Lecture-style class (LC)</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td><strong>1. Learning builds on prior knowledge.</strong></td>
-                        <td>Questions within activities are designed to elicit and build on students’ initial ideas.</td>
-                        <td>Similar to SC.</td>
-                    </tr>
-                    <tr>
-                        <td><strong>2. Learning is a complex process.</strong></td>
-                        <td>Big ideas/science practices developed within & across units. A variety of question types guide students’ thinking. Extensions (HW) are computer-based tutorials that include online quizzes.</td>
-                        <td>Similar to SC, except students do not write their own explanations; instead they evaluate carefully crafted explanations for accuracy. Extensions are the same as SC</td>
-                    </tr>
-                    <tr>
-                        <td><strong>3. Learning is facilitated through interaction with tools.</strong>
-                        </td>
-                        <td>Students use hands-on materials, data acquisition tools and simulations, and answer questions on activity sheets.</td>
-                        <td>Students watch videos of experiments, demos and simulations, answer questions with ‘clickers’ and on lesson sheets.</td>
-                    </tr>
-                    <tr>
-                        <td><strong>4. Learning is facilitated through interactions with others</strong>
-                        </td>
-                        <td>Students engage in small group and whole class discussions.</td>
-                        <td>Students discuss thinking with neighbors, limited sharing with whole class.</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <strong>5. Learning is facilitated through establishment of certain specific behavioral practices and expectations.</strong>
-                        </td>
-                        <td>Written prompts/instructor comments support expectations of providing evidence, active participation, and responsibility for learning.</td>
-                        <td>Similar to SC except the degree of participation is less and students are expected to reach consensus at end of each lesson.</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        
-        <p>
-        To learn more about <em>Next Gen PET</em>'s Flexibility,
+        <em>Next Gen PET</em>
+        and its predecessor curricula was guided by five design principles based on research on learning. The following table shows how the design principles are put into action in
+        <em>Next Gen PET</em>
+        .
+    </p>
+    <div class="table-responsive">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Design Principles</th>
+                    <th>Studio-style class (SC)</th>
+                    <th>Lecture-style class (LC)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <strong>1. Learning builds on prior knowledge.</strong>
+                    </td>
+                    <td>Questions within activities are designed to elicit and build on students’ initial ideas.</td>
+                    <td>Similar to SC.</td>
+                </tr>
+                <tr>
+                    <td>
+                        <strong>2. Learning is a complex process.</strong>
+                    </td>
+                    <td>Big ideas/science practices developed within & across units. A variety of question types guide students’ thinking. Extensions (HW) are computer-based tutorials that include online quizzes.</td>
+                    <td>Similar to SC, except students do not write their own explanations; instead they evaluate carefully crafted explanations for accuracy. Extensions are the same as SC</td>
+                </tr>
+                <tr>
+                    <td>
+                        <strong>3. Learning is facilitated through interaction with tools.</strong>
+                    </td>
+                    <td>Students use hands-on materials, data acquisition tools and simulations, and answer questions on activity sheets.</td>
+                    <td>Students watch videos of experiments, demos and simulations, answer questions with ‘clickers’ and on lesson sheets.</td>
+                </tr>
+                <tr>
+                    <td>
+                        <strong>4. Learning is facilitated through interactions with others</strong>
+                    </td>
+                    <td>Students engage in small group and whole class discussions.</td>
+                    <td>Students discuss thinking with neighbors, limited sharing with whole class.</td>
+                </tr>
+                <tr>
+                    <td>
+                        <strong>5. Learning is facilitated through establishment of certain specific behavioral practices and expectations.</strong>
+                    </td>
+                    <td>Written prompts/instructor comments support expectations of providing evidence, active participation, and responsibility for learning.</td>
+                    <td>Similar to SC except the degree of participation is less and students are expected to reach consensus at end of each lesson.</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
+    <p>
+        To learn more about
+        <em>Next Gen PET</em>
+        's Flexibility,
         <a href="{{ url('about/flexibility') }}">click here</a>
-        </p>
-        Get back to main page,
-        <a href="{{ url('/') }}">click here</a>
-        </p>
-    </section>
+    </p>
+    <p>
+    Get back to main page,
+    <a href="{{ url('/') }}">click here</a>
+    </p>
+
 
 @stop
