43480e5ecbf2cf03d1995ae813beca4501b6f913
Henry Garcia <hjgarcia1@gmail.com>
2017-03-30 10:58:37 -0400

    added nextgen pet logo to the footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -15,6 +15,12 @@
             <div class="row">
                 <div class="col-sm hidden-sm-down"></div>
                 <div class="col-sm text-center align-self-center">
+                    <img src="{{ asset('img/nextgenpetlogo.png') }}"
+                         class="center-block img-responsive"
+                         alt="NextGEN PET"
+                         width="95">
+                </div>
+                <div class="col-sm text-center align-self-center">
                     <img src="{{ asset('img/nsf.png') }}"
                          class="center-block img-responsive"
                          alt="National Science Foundation"
