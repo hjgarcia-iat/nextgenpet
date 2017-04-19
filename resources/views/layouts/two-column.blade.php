ef38accbaa6cd56fce93416c5169b5c102097482
Henry Garcia <hjgarcia1@gmail.com>
2017-04-19 16:08:28 -0400

    auth check fix

--- resources/views/layouts/two-column.blade.php
+++ resources/views/layouts/two-column.blade.php
@@ -14,12 +14,15 @@
                 <div class="row">
                     <div class="col-md-12 d-md-flex flex-md-row align-items-md-center">
                         <h2 class="mb-md-0">@yield('pageTitle')</h2>
-                        <div class="ml-md-auto">
-                            <p class="text-muted mb-0">
-                                <small>Logged in as:
-                                    <a href="{{ route('my-account') }}">{{ auth()->user()->name }}</a></small>
-                            </p>
-                        </div>
+                        @if(auth()->check())
+                            <div class="ml-md-auto">
+                                <p class="text-muted mb-0">
+                                    <small>Logged in as:
+                                        <a href="{{ route('my-account') }}">{{ auth()->user()->name }}</a>
+                                    </small>
+                                </p>
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
