be1d6b88c912d20668b30a467c5d96593c3505b9
Henry Garcia <hjgarcia1@gmail.com>
2017-04-19 13:27:04 -0400

    added user's name to pagetitle area of the site

--- resources/views/layouts/two-column.blade.php
+++ resources/views/layouts/two-column.blade.php
@@ -11,7 +11,17 @@
         @yield('banner')
         <section class="page-title">
             <div class="container">
-                <h2>@yield('pageTitle')</h2>
+                <div class="row">
+                    <div class="col-md-12 d-md-flex flex-md-row align-items-md-center">
+                        <h2 class="mb-md-0">@yield('pageTitle')</h2>
+                        <div class="ml-md-auto">
+                            <p class="text-muted mb-0">
+                                <small>Logged in as:
+                                    <a href="{{ route('my-account') }}">{{ auth()->user()->name }}</a></small>
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
         <section class="page-content-wrapper">
