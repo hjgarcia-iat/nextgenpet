2868c177d692c7d2a3b4955d5fc21f3b4b675d94
Henry Garcia <hjgarcia1@gmail.com>
2017-04-21 15:57:29 -0400

    page demo complete

--- resources/views/pages/lecture-style-class/instructor/magnetism-and-static-electricity-module.blade.php
+++ resources/views/pages/lecture-style-class/instructor/magnetism-and-static-electricity-module.blade.php
@@ -10,29 +10,108 @@
     @include('pages.lecture-style-class._banner')
 @stop
 @section('content')
-
     <section id="pdf-content" class="pdf-content"></section>
     <section id="html-content" class="html-content">
-        <h3>Magnetism and Static Electricity Module</h3>
-        <p>
-            The main purpose of this module is to engage students in several science and engineering practices, particularly developing and using models, constructing explanations and engaging in argument from evidence.  The explanatory models students develop are mechanistic, so students get extensive experience with the crosscutting concept of cause and effect: mechanism and explanation.   As students are engaged in the practices, what emerges is an understanding of magnetic and electrostatic interactions, which are part of the core physical science idea PS2B, types of interactions.
-        </p>
-        <p>
-            The first unit in the module focuses on developing a model of magnetism. Once students have experience with the model development process, they then work through the second unit, where developing a model for static electricity is more challenging.  Whereas there is only one type of magnetic material, there are basically two types of materials involved in static electric effects: insulators and conductors. 
-        </p>
-        <h4>Unit M: Developing a Model for Magnetism</h4>
-        <p>
-            Unit M focuses on helping students develop a model that can successfully explain many different observations involving magnets. This unit provides a nice example of how models are proposed, tested and modified based on evidence. Most students will change their model several times during the unit as they keep obtaining evidence that does not support their current model. By the end of the unit, however, the class will come to consensus on a ‘small-magnet alignment model,’ which can explain a wide range of magnetism phenomena. It has many features in common with the Domain Model. In this unit students can perform some simple hands-on experiments at their desktops if materials are available, or can watch movies of all the experiments.
-        </p>
-        @include('pages.partials._canvas',['canvasId' => 'canvas'])
-        <h4>Unit SE: Developing a Model for Static Electricity </h4>
-        <p>
-            Unit SE guides students through the development of a model for electric charges in materials that can explain the interactions between charged and uncharged objects, and between two charged objects; and includes both conductors and non-conductors. In this unit students can perform some simple hands-on experiments and then watch movies of other experiments (more challenging to do), or they can watch movies of all the experiments.
-        </p>
-        <p>
-            The unit begins by having students explore various static electric effects and compare the static electric interaction with the magnetic interaction. Students then work through two extensions where they learn about the triboelectric series and develop an understanding of the law of electric charges. In the second lesson students propose their initial model (with + and – charges) to explain the charging of two non-conductors by rubbing, and explore the interaction of a soda can electroscope with charged and uncharged objects. In the third lesson they consider how to describe and represent uncharged objects in their model, and extend this in an extension. In the next extension they learn about a simple atomic model and which type of charge (plus or minus) is actually transferred by rubbing two uncharged non-conductors together. In the fourth lesson and two associated extensions they extend their model for both conducting and non-conducting materials and learn about discharging. By this time the class has developed a plausible model for electric charges in materials. In the fifth lesson students apply their model to explain some interesting phenomena.
-        </p>
-        @include('pages.partials._canvas',['canvasId' => 'canvas2'])
+        <h2>Magnetism and Electricty Module</h2>
+        <h4>Magnetism &amp; Static Electricity Module</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A asperiores beatae dolores est hic laborum minus
+            provident reiciendis temporibus voluptas? Aliquam animi cumque ducimus fugit nobis non numquam optio
+            similique.</p>
+        <ul class="fa fa-ul">
+            <li>
+                <i class="fa-li fa fa-file-word-o"></i> <a href="">Module MSE Test Bank</a>
+            </li>
+            <li>
+                <i class="fa-li fa fa-file-word-o"></i> <a href="">Module Equipment List</a>
+            </li>
+        </ul>
+        <h5>Developing a Model for Magnetism Unit</h5>
+        <div class="well m-0 mb-4 p-4">
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-pdf-o"></i> <a href="">Unit Trajectory</a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-pdf-o"></i> <a href="">Activity and Extension Summaries </a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-pdf-o"></i> <a href="">Lesson Keys for Whole Unit </a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-pdf-o"></i> <a href="">Student Materials for Whole Unit </a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i> <a href="">Extension quizzes for whole unit </a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-word-o"></i> <a href="">Scientist's Ideas</a>
+                </li>
+            </ul>
+        </div>
+
+
+        <div id="accordion" role="tablist" aria-multiselectable="true">
+            <div class="card">
+                <div class="card-header" role="tab" id="headingOne">
+                    <h5 class="mb-0">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Lesson 1: Modeling &amp; the Mystery Tube</a>
+                    </h5>
+                </div>
+                <div id="collapseOne" class="collapse show" role="tabpanel" aria-labelledby="headingOne">
+                    <div class="card-block">
+                        <ul class="fa fa-ul">
+                            <li>
+                                <i class="fa-li fa fa-file-word-o"></i> <a href="">Link 1</a>
+                            </li>
+                            <li>
+                                <i class="fa-li fa fa-file-word-o"></i> <a href="">Link 2</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header" role="tab" id="headingTwo">
+                    <h5 class="mb-0">
+                        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Lesson 2: Exploring Magnetic Effects</a>
+                    </h5>
+                </div>
+                <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo">
+                    <div class="card-block">
+                        <ul class="fa fa-ul">
+                            <li>
+                                <i class="fa-li fa fa-file-word-o"></i> <a href="">Lesson Key</a>
+                            </li>
+                            <li>
+                                <i class="fa-li fa fa-file-powerpoint-o"></i> <a href="">Instructor Slides</a>
+                            </li>
+                            <li>
+                                <i class="fa-li fa fa-file-word-o"></i> <a href="">Student Materials</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header" role="tab" id="headingThree">
+                    <h5 class="mb-0">
+                        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">Extension
+                            A: Exploring the Region Around a Magnet </a>
+                    </h5>
+                </div>
+                <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree">
+                    <div class="card-block">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                        3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
+                        laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin
+                        coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes
+                        anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings
+                        occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard
+                        of them accusamus labore sustainable VHS.
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 @stop
 
@@ -43,6 +122,5 @@
 @stop
 
 @section('styles')
-    <link rel="stylesheet"
-          href="{{ asset('css/vis.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/vis.min.css') }}">
 @stop
