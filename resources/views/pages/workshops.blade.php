8064a67137d8a99cbb5b2cdc145b17839945a3d5
Henry Garcia <hjgarcia1@gmail.com>
2017-04-21 13:29:14 -0400

    more typo fixes

--- resources/views/pages/workshops.blade.php
+++ resources/views/pages/workshops.blade.php
@@ -17,8 +17,8 @@
     <p>2017 Physics Teacher Education Coalition (PhysTEC) Conference, Atlanta, GA February 17-18, 2017. A half-day
         workshop on Next Gen PET and the associated Faculty Online Learning Community (Next Gen FOLC) will be offered at
         the pre-conference session from 1:00 pm– 5:00 pm, February 16, 2017. Information about the
-        <a href="http://www.phystec.org/conferences/2017/" target="_blank">PhysTEC Conference</a> , and information
-        about the <a href="http://www.ngpfolc.org" target="_blank">NGP FOLC</a> . </p>
+        <a href="http://www.phystec.org/conferences/2017/" target="_blank">PhysTEC Conference</a>, and information
+        about the <a href="http://www.ngpfolc.org" target="_blank">NGP FOLC</a>. </p>
 
 
 @stop
