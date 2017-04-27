437bf73a329295a92d31b6b101bc2c959ce275aa
Henry Garcia <hjgarcia1@gmail.com>
2017-04-27 16:07:11 -0400

    module ie first unit complete

--- resources/views/pages/lecture-style-class/instructor/interactions-and-energy-module.blade.php
+++ resources/views/pages/lecture-style-class/instructor/interactions-and-energy-module.blade.php
@@ -1,8 +1,5 @@
 @extends('layouts.two-column')
-
-@section('pageTitle')
-    Lecture Style Class
-@stop
+@section('pageTitle','Lecture Style Class')
 @section('sidebar')
     @include('pages.lecture-style-class._sidebar')
 @stop
@@ -10,35 +7,91 @@
     @include('pages.lecture-style-class._banner')
 @stop
 @section('content')
-
     <section id="pdf-content" class="pdf-content"></section>
     <section id="html-content" class="html-content">
-        <h3>Interactions and Energy Module</h3>
-        <p>The main purpose of this module is to introduce students both to various types of interactions between objects and to the energy model for describing those interactions. In the first unit (energy model) students are introduced to contact push/pull interactions, heat interactions (due to temperature differences), electric circuit interactions, light interactions and sound interactions. In the second unit (potential energy and fields) students are introduced to elastic interactions; magnetic, static electric and electromagnetic interactions; and gravitational interactions.
-        </p>
-        <p>The energy model is represented by a giver/receiver energy diagram, in which during the interaction ‘energy’ is transferred from the ‘giver’ to the ‘receiver,’ the giver decreases in some type of energy and the receiver increases in some type (same or different) of energy. We use only the generic term ‘energy’ to label the actual transfer in the diagram because the transfer is not related to an observable quantity. However, since changes in the energy giver and receiver are directly related to observable or measurable changes in some quantity, those energy changes are given specific names. Thus, the energy associated with changes in speed is called kinetic energy; the energy change associated with changes in temperature is called thermal energy; the energy changes associated with changes in the distance between an object and the earth is called gravitational potential energy; and so forth. Inherent in the giver/receiver energy diagram is a visual representation of the Law of the Conservation of Energy.
-        </p>
-        <h4>Unit EM: Energy-based Model for Interactions</h4>
-        <p>
-            Unit EM focuses on introducing students to the energy model for describing interactions. Students are first shown a simple giver/receiver energy diagram to represent the contact push/pull interaction between two colliding carts. From there, through successive lessons students learn about additional types of interactions (light, heat, electric circuit) and additional forms of energy (thermal, chemical potential energy). The corresponding G/R energy diagrams become more complex, describing the flow of energy through a system of interacting objects. Initially, all energy diagrams are qualitative, simply describing the flow of energy through a system of interacting objects. When students learn about energy efficiency, they are able to quantify the G/R energy diagrams by calculating the energy amounts corresponding to each part of the diagram. By the end of the unit the students are expected to describe a chain of interactions between several objects (mainly in an electric circuit) by drawing the appropriate G/R energy diagram, quantifying it (assuming they know how much energy was available at the start), calculating the overall efficiency of the system, and validating the conservation of energy quantitatively.
+        <h2>Interactions &amp; Energy Module</h2>
+        <ul class="fa fa-ul">
+            <li>
+                <i class="fa-li fa fa-file-word-o"></i>
+                <a href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/interactions-energy-module/module-ie-test-bank.docx']) }}" target="_blank">Module
+                    IE Test Bank</a>
+            </li>
+        </ul>
 
-        </p>
-        @include('pages.partials._canvas',['canvasId' => 'canvas'])
-        
-        <h4>Unit PEF: Potential Energy and Fields</h4>
-        <p>
-            As the name indicates, Unit PEF focuses on potential energy and fields. The first lesson introduces students to the interaction between an elastic object (rubber band or spring) and another object. Students learn about elastic potential energy and draw energy diagrams describing the interactions. In the next lesson students are introduced to magnetic and static electric interactions; that is, how magnets affect each other and how electrically charged objects affect each other. This lesson is optional; if students have already studied those interactions in the Magnetism and Static Electricity Module then the lesson can be skipped. Otherwise, students need to learn about those interactions in preparation for the following lessons. Lesson 3 introduces ‘action-at-a-distance’ interactions, and how to describe them in terms of energy. Specifically, students learn how to describe the magnetism and static electric interactions using giver/receiver energy diagrams. They learn about magnetic fields and magnetic potential energy, and about electric fields and electric potential energy. In the fourth lesson they are introduced to the gravitational interaction and gravitational potential energy. Finally, in Lesson 5 they learn about electromagnetic interactions: how moving electric charges can create a magnetic field and how moving magnets can create an electric field.
-        </p>
-        @include('pages.partials._canvas',['canvasId' => 'canvas2'])
+        <ul class="nav nav-tabs" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link active" data-toggle="tab" href="#unit-1">Energy-based Model for Interactions</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" href="#unit-2">Potential Energy and Fields</a>
+            </li>
+        </ul>
+        <div class="tab-content">
+            <div class="tab-pane pt-3 pb-3 active" id="unit-1" role="tabpanel">
+                <ul class="fa fa-ul">
+                    <li>
+                        <i class="fa-li fa fa-file-pdf-o"></i>
+                        <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/interactions-energy-module/1-unit-em-energy-based-model-for-interactions/unit-em-trajectory.pdf']) }}">Unit
+                            Trajectory</a>
+                    </li>
+                    <li>
+                        <i class="fa-li fa fa-file-pdf-o"></i>
+                        <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/interactions-energy-module/1-unit-em-energy-based-model-for-interactions/unit-em-lesson-and-extension-summaries.pdf']) }}">Activity
+                            and Extension Summaries </a>
+                    </li>
+                    <li>
+                        <i class="fa-li fa fa-file-pdf-o"></i>
+                        <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/interactions-energy-module/1-unit-em-energy-based-model-for-interactions/unit-em-keys.pdf']) }}">Lesson
+                            Keys for Whole Unit </a>
+                    </li>
+                    <li>
+                        <i class="fa-li fa fa-file-pdf-o"></i>
+                        <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/interactions-energy-module/1-unit-em-energy-based-model-for-interactions/unit-em-student-materials.pdf']) }}">Student
+                            Materials for Whole Unit </a>
+                    </li>
+                    <li>
+                        <i class="fa-li fa fa-file-powerpoint-o"></i>
+                        <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/interactions-energy-module/1-unit-em-energy-based-model-for-interactions/unit-em-extension-quiz-keys.pptx']) }}">Extension
+                            quizzes for whole unit </a>
+                    </li>
+                </ul>
+                <div id="accordion" role="tablist" aria-multiselectable="true">
+                    @include('pages.lecture-style-class.partials.instructor.ie.em')
+                </div>
+            </div>
+            <div class="tab-pane pt-3 pb-3" id="unit-2" role="tabpanel">
+
+                <ul class="fa fa-ul">
+                    <li>
+                        <i class="fa-li fa fa-file-pdf-o"></i>
+                        <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/unit-se-trajectory.pdf']) }}">Unit
+                            Trajectory</a>
+                    </li>
+                    <li>
+                        <i class="fa-li fa fa-file-pdf-o"></i>
+                        <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/unit-se-lesson-and-extension-summaries.pdf']) }}">Activity
+                            and Extension Summaries </a>
+                    </li>
+                    <li>
+                        <i class="fa-li fa fa-file-pdf-o"></i>
+                        <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/unit-se-lesson-keys.pdf']) }}">Lesson
+                            Keys for Whole Unit </a>
+                    </li>
+                    <li>
+                        <i class="fa-li fa fa-file-pdf-o"></i>
+                        <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/unit-se-student-materials.pdf']) }}">Student
+                            Materials for Whole Unit </a>
+                    </li>
+                    <li>
+                        <i class="fa-li fa fa-file-powerpoint-o"></i>
+                        <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/unit-se-extension-quiz-keys.pptx']) }}">Extension
+                            quizzes for whole unit </a>
+                    </li>
+                </ul>
+                <div id="accordionTwo" role="tablist" aria-multiselectable="true">
+                    @include('pages.lecture-style-class.partials.instructor.ie.pef')
+                </div>
+            </div>
+        </div>
     </section>
-@stop
-
-@section('scripts')
-    <script src="{{ asset('js/vis.min.js') }}"></script>
-    <script src="{{ asset('js/contentjs/lecture-ie.js') }}"></script>
-@stop
-
-@section('styles')
-    <link rel="stylesheet"
-          href="{{ asset('css/vis.min.css') }}">
 @stop
---
+++ resources/views/pages/lecture-style-class/partials/instructor/ie/pef.blade.php
@@ -0,0 +1,254 @@
+<div class="card">
+    <div class="card-header" role="tab" id="headingTen">
+        <h5 class="mb-0">
+            <a data-toggle="collapse" data-parent="#accordionTwo" href="#collapseTen" aria-expanded="true" aria-controls="collapseTen">Lesson
+                1: Exploring Static Electric Effects</a>
+        </h5>
+    </div>
+    <div id="collapseTen" class="collapse" role="tabpanel" aria-labelledby="headingTen">
+        <div class="card-block">
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/1-l1-exploring-static-electric-effects/use-l1-instructors-slides.pptx']) }}">Instructor
+                        Slides</a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-word-o"></i>
+                    <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/1-l1-exploring-static-electric-effects/use-l1-student-materials.docx']) }}">Student
+                        Materials</a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-word-o"></i>
+                    <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/1-l1-exploring-static-electric-effects/use-l1-key.docx']) }}">Key</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="card">
+    <div class="card-header" role="tab" id="headingEleven">
+        <h5 class="mb-0">
+            <a class="collapsed" data-toggle="collapse" data-parent="#accordionTwo" href="#collapseEleven" aria-expanded="false" aria-controls="collapseEleven">Ext
+                A: Which Charge is Which?</a>
+        </h5>
+    </div>
+    <div id="collapseEleven" class="collapse" role="tabpanel" aria-labelledby="headingEleven">
+        <div class="card-block">
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/2-ext-a-which-charge-is-which/use-exta-extension-mock-up.pptx']) }}">Extra
+                        Extension Mockup Slides</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="card">
+    <div class="card-header" role="tab" id="headingTwelve">
+        <h5 class="mb-0">
+            <a class="collapsed" data-toggle="collapse" data-parent="#accordionTwo" href="#collapseTwelve" aria-expanded="false" aria-controls="collapseTwelve">Ext
+                B: The Law of Electric Charges</a>
+        </h5>
+    </div>
+    <div id="collapseTwelve" class="collapse" role="tabpanel" aria-labelledby="headingTwelve">
+        <div class="card-block">
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/3-ext-b-the-law-of-electric-charges/use-extb-extension-mock-up.pptx']) }}">Extra
+                        Extension Mockup Slides</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="card">
+    <div class="card-header" role="tab" id="headingThirteen">
+        <h5 class="mb-0">
+            <a class="collapsed" data-toggle="collapse" data-parent="#accordionTwo" href="#collapseThirteen" aria-expanded="false" aria-controls="collapseThirteen">Lesson
+                2: Developing a Model for Static Electricity</a>
+        </h5>
+    </div>
+    <div id="collapseThirteen" class="collapse" role="tabpanel" aria-labelledby="headingThirteen">
+        <div class="card-block">
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/4-l2-developing-a-model-for-static-electricity/use-l2-instructors-slides.pptx']) }}">Instructor
+                        Slides</a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-word-o"></i>
+                    <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/4-l2-developing-a-model-for-static-electricity/use-l2-student-materials.docx']) }}">Student
+                        Materials</a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-word-o"></i>
+                    <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/4-l2-developing-a-model-for-static-electricity/use-l2-key.docx']) }}">Key</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="card">
+    <div class="card-header" role="tab" id="headingFourteen">
+        <h5 class="mb-0">
+            <a class="collapsed" data-toggle="collapse" data-parent="#accordionTwo" href="#collapseFourteen" aria-expanded="false" aria-controls="collapseFourteen">Lesson
+                3: Representing Uncharged Objects in Your Model</a>
+        </h5>
+    </div>
+    <div id="collapseFourteen" class="collapse" role="tabpanel" aria-labelledby="headingFourteen">
+        <div class="card-block">
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/5-l3-represening-uncharged-objects-in-your-model/use-l3-instructors-slides.pptx']) }}">Instructor
+                        Slides</a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-word-o"></i>
+                    <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/5-l3-represening-uncharged-objects-in-your-model/use-l3-student-materials.docx']) }}">Student
+                        Materials</a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-word-o"></i>
+                    <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/5-l3-represening-uncharged-objects-in-your-model/use-l3-key.docx']) }}">Key</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="card">
+    <div class="card-header" role="tab" id="headingSixteen">
+        <h5 class="mb-0">
+            <a class="collapsed" data-toggle="collapse" data-parent="#accordionTwo" href="#collapseSixteen" aria-expanded="false" aria-controls="collapseSixteen">Ext
+                C: Electroscope and Negatively (–) Charged Object</a>
+        </h5>
+    </div>
+    <div id="collapseSixteen" class="collapse" role="tabpanel" aria-labelledby="headingSixteen">
+        <div class="card-block">
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/6-ext-c-electroscope-and-negatively-charged-objects/use-extc-extension-mock-up.pptx']) }}">Extra
+                        Extension Mockup Slides</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="card">
+    <div class="card-header" role="tab" id="headingSeventeen">
+        <h5 class="mb-0">
+            <a class="collapsed" data-toggle="collapse" data-parent="#accordionTwo" href="#collapseSeventeen" aria-expanded="false" aria-controls="collapseSeventeen">Ext
+                D: Nature of Charged Entities</a>
+        </h5>
+    </div>
+    <div id="collapseSeventeen" class="collapse" role="tabpanel" aria-labelledby="headingSeventeen">
+        <div class="card-block">
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/7-ext-d-nature-of-charged-entities/use-extd-extension-mock-up.pptx']) }}">Extra
+                        Extension Mockup Slides</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="card">
+    <div class="card-header" role="tab" id="headingEighteen">
+        <h5 class="mb-0">
+            <a class="collapsed" data-toggle="collapse" data-parent="#accordionTwo" href="#collapseEighteen" aria-expanded="false" aria-controls="collapseEighteen">Lesson
+                4: Refining Your Model for Different Materials</a>
+        </h5>
+    </div>
+    <div id="collapseEighteen" class="collapse" role="tabpanel" aria-labelledby="headingEighteen">
+        <div class="card-block">
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/8-l4-refining-your-model-for-different-materials/use-l4-instructors-slides.pptx']) }}">Instructor
+                        Slides</a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-word-o"></i>
+                    <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/8-l4-refining-your-model-for-different-materials/use-l4-student-materials.docx']) }}">Student
+                        Materials</a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-word-o"></i>
+                    <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/8-l4-refining-your-model-for-different-materials/use-l4-key.docx']) }}">Key</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="card">
+    <div class="card-header" role="tab" id="headingNineteen">
+        <h5 class="mb-0">
+            <a class="collapsed" data-toggle="collapse" data-parent="#accordionTwo" href="#collapseNineteen" aria-expanded="false" aria-controls="collapseNineteen">Ext
+                E: Discharging Charged Objects</a>
+        </h5>
+    </div>
+    <div id="collapseNineteen" class="collapse" role="tabpanel" aria-labelledby="headingNineteen">
+        <div class="card-block">
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/9-ext-e-discharging-charged-objects/use-exte-extension-mock-up.pptx']) }}">Extra
+                        Extension Mockup Slides</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="card">
+    <div class="card-header" role="tab" id="headingTwenty">
+        <h5 class="mb-0">
+            <a class="collapsed" data-toggle="collapse" data-parent="#accordionTwo" href="#collapseTwenty" aria-expanded="false" aria-controls="collapseTwenty">Ext
+                F: Charged and Uncharged Objects</a>
+        </h5>
+    </div>
+    <div id="collapseTwenty" class="collapse" role="tabpanel" aria-labelledby="headingTwenty">
+        <div class="card-block">
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/10-ext-f-charged-and-uncharged-objects/use-extf-extension-mock-up.pptx']) }}">Extra
+                        Extension Mockup Slides</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="card">
+    <div class="card-header" role="tab" id="headingTwentyone">
+        <h5 class="mb-0">
+            <a class="collapsed" data-toggle="collapse" data-parent="#accordionTwo" href="#collapseTwentyone" aria-expanded="false" aria-controls="collapseTwentyone">Lesson
+                5: Explaining Phenomena Involving Static Electricity</a>
+        </h5>
+    </div>
+    <div id="collapseTwentyone" class="collapse" role="tabpanel" aria-labelledby="headingTwentyone">
+        <div class="card-block">
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/11-l5-explaining-phenomena-involving-static-electrcity/use-l5-instructors-slides.pptx']) }}">Instructor
+                        Slides</a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-word-o"></i>
+                    <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/11-l5-explaining-phenomena-involving-static-electrcity/use-l5-student-materials.docx']) }}">Student
+                        Materials</a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-word-o"></i>
+                    <a target="_blank" href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/magnetism-static-electricty-module/unit-se-developing-a-model-for-static-electricity/11-l5-explaining-phenomena-involving-static-electrcity/use-l5-key.docx']) }}">Key</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
