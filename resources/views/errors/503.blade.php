e29b537ca0290d3f00296f2a5be2347e8fa26b77
Henry Garcia <hjgarcia1@gmail.com>
2017-05-18 14:55:34 -0400

    new 503 view

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -4,6 +4,10 @@
 
 @section('content')
     <div class="well well-error">
+        <p class="text-center">
+            <img src="{{ asset('img/logo.png') }}"
+                 alt="It's About Time">
+        </p>
         <h2 class="text-center">Be right back!</h2>
         <p class="lead text-center">We are making changes to the website</p>
     </div>
