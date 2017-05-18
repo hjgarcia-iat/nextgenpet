5401d4561858ed3ac0cce8f3f7fdf52a00591bf4
Henry Garcia <hjgarcia1@gmail.com>
2017-05-18 14:45:53 -0400

    changed mail settings

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'contact@iat.com',
+        'name' => 'It\'s About Time',
     ],
 
     /*
