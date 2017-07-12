f861b69eec5ed84ccc624f91ce5c9977946db10b
Henry Garcia <hjgarcia1@gmail.com>
2017-07-11 20:05:11 -0400

    teaching and learning pages mockups complete

---
+++ resources/views/pages/lecture-style-class/partials/instructor/tl/content-module-clusters.blade.php
@@ -0,0 +1,149 @@
+<div class="card">
+    <div class="card-header" role="tab" id="headingOne-b">
+        <h5 class="mb-0">
+            <a data-toggle="collapse" data-parent="#accordionTwo" href="#collapseOne-b" aria-expanded="true"
+               aria-controls="collapseOne-b">Module MSE Cluster</a>
+        </h5>
+    </div>
+    <div id="collapseOne-b" class="collapse" role="tabpanel" aria-labelledby="headingOne-b">
+        <div class="card-block">
+    
+            <p><strong>Extension D: Interviewing children about Magnetism</strong></p>
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/waves-sound-light-module/2-unit-l-light-and-color/5-l3-refraction-of-light/ul-l3-instructors-slides.pptx']) }}">Link
+                        1</a>
+                </li>
+            </ul>
+            <p><strong>Extension E: Teaching and Learning Engineering Design</strong></p>
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/waves-sound-light-module/2-unit-l-light-and-color/5-l3-refraction-of-light/ul-l3-instructors-slides.pptx']) }}">Link
+                        1</a>
+                </li>
+            </ul>
+            <p><strong>Lesson 2: Models, Magnets &amp; Children's Ideas</strong></p>
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/waves-sound-light-module/2-unit-l-light-and-color/5-l3-refraction-of-light/ul-l3-instructors-slides.pptx']) }}">Link
+                        1</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="card">
+    <div class="card-header" role="tab" id="headingTwo-b">
+        <h5 class="mb-0">
+            <a class="collapsed" data-toggle="collapse" data-parent="#accordionTwo" href="#collapseTwo-b"
+               aria-expanded="false" aria-controls="collapseTwo-b">Module IE Cluster</a>
+        </h5>
+    </div>
+    <div id="collapseTwo-b" class="collapse" role="tabpanel" aria-labelledby="headingTwo-b">
+        <div class="card-block">
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/waves-sound-light-module/2-unit-l-light-and-color/2-exta-drawing-light-ray-diagrams-for-pinholes-shadows/ul-exta-extension-mock-up.pptx']) }}">Extension
+                        Mockup Slides</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="card">
+    <div class="card-header" role="tab" id="headingThree-b">
+        <h5 class="mb-0">
+            <a class="collapsed" data-toggle="collapse" data-parent="#accordionTwo" href="#collapseThree-b"
+               aria-expanded="false" aria-controls="collapseThree-b">Module IF Cluster</a>
+        </h5>
+    </div>
+    <div id="collapseThree-b" class="collapse" role="tabpanel" aria-labelledby="headingThree-b">
+        <div class="card-block">
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/waves-sound-light-module/2-unit-l-light-and-color/3-l2-reflection-of-light/ul-l2-instructors-slides.pptx']) }}">Instructor
+                        Slides</a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-word-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/waves-sound-light-module/2-unit-l-light-and-color/3-l2-reflection-of-light/ul-l2-student-materials.docx']) }}">Student
+                        Materials</a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-word-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/waves-sound-light-module/2-unit-l-light-and-color/3-l2-reflection-of-light/ul-l2-key.docx']) }}">Key</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="card">
+    <div class="card-header" role="tab" id="headingFour-b">
+        <h5 class="mb-0">
+            <a class="collapsed" data-toggle="collapse" data-parent="#accordionTwo" href="#collapseFour-b"
+               aria-expanded="false" aria-controls="collapseFour-b">Module WSL Cluster</a>
+        </h5>
+    </div>
+    <div id="collapseFour-b" class="collapse" role="tabpanel" aria-labelledby="headingFour-b">
+        <div class="card-block">
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/waves-sound-light-module/2-unit-l-light-and-color/4-extb-more-on-mirror-reflections-and-images/ul-extb-extension-mock-up.pptx']) }}">Extension
+                        Mockup Slides</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="card">
+    <div class="card-header" role="tab" id="headingFive-b">
+        <h5 class="mb-0">
+            <a class="collapsed" data-toggle="collapse" data-parent="#accordionTwo" href="#collapseFive-b"
+               aria-expanded="false" aria-controls="collapseFive-b">Module MI Cluster</a>
+        </h5>
+    </div>
+    <div id="collapseFive-b" class="collapse" role="tabpanel" aria-labelledby="headingFive-b">
+        <div class="card-block">
+            <h5>Extension D: Interviewing children about Magnetism</h5>
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/waves-sound-light-module/2-unit-l-light-and-color/5-l3-refraction-of-light/ul-l3-instructors-slides.pptx']) }}">Link 1</a>
+                </li>
+            </ul>
+            <h5>Extension E: Teaching and Learning Engineering Design</h5>
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/waves-sound-light-module/2-unit-l-light-and-color/5-l3-refraction-of-light/ul-l3-instructors-slides.pptx']) }}">Link
+                        1</a>
+                </li>
+            </ul>
+            <h5>Lesson 2: Models, Magnets &amp; Children's Ideas</h5>
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/lecture-style/waves-sound-light-module/2-unit-l-light-and-color/5-l3-refraction-of-light/ul-l3-instructors-slides.pptx']) }}">Link
+                        1</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
