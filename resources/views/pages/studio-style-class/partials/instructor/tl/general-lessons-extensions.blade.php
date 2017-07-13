ef5c5495f94a1209f0250d3b2283ff26c3949fdf
Henry Garcia <hjgarcia1@gmail.com>
2017-07-13 15:35:39 -0400

    added new studio style teaching and learning page

---
+++ resources/views/pages/studio-style-class/partials/instructor/tl/general-lessons-extensions.blade.php
@@ -0,0 +1,149 @@
+<div class="card">
+    <div class="card-header" role="tab" id="headingOne">
+        <h5 class="mb-0">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true"
+               aria-controls="collapseOne">Introductory Cluster</a>
+        </h5>
+    </div>
+    <div id="collapseOne" class="collapse show" role="tabpanel" aria-labelledby="headingOne">
+        <div class="card-block">
+
+            <p><strong>Extension A: NGSS and Next Gen PET</strong></p>
+
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-word-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/teaching-and-learning/lecture-style/1-general-lessons-and-extensions/1-exta-ngss-next-gen-pet/tl-ext-a-reading.docx']) }}">Reading
+                        (word)</a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-pdf-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/teaching-and-learning/lecture-style/1-general-lessons-and-extensions/1-exta-ngss-next-gen-pet/tl-ext-a-reading.pdf']) }}">Reading
+                        (PDF)</a>
+                </li>
+            </ul>
+
+            <p><strong>Extension B: Engineering the NGSS</strong></p>
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-word-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/teaching-and-learning/lecture-style/1-general-lessons-and-extensions/2-extb-engineering-the-ngss/tl-ext-b-reading.docx']) }}">Reading
+                        (word)</a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-pdf-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/teaching-and-learning/lecture-style/1-general-lessons-and-extensions/2-extb-engineering-the-ngss/tl-ext-b-reading.pdf']) }}">Reading
+                        (PDF)</a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-word-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/teaching-and-learning/lecture-style/1-general-lessons-and-extensions/2-extb-engineering-the-ngss/tl-ext-a-b-quiz.docx']) }}">Quiz</a>
+                </li>
+            </ul>
+            <p><strong>Lesson 1: Teaching &amp; Learning Science</strong></p>
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/teaching-and-learning/lecture-style/1-general-lessons-and-extensions/3-l1-teaching-learning-science/tl-l1-instructors-slides.pptx']) }}">Instructor
+                        Slides</a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-word-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/teaching-and-learning/lecture-style/1-general-lessons-and-extensions/3-l1-teaching-learning-science/tl-l1-key.docx']) }}">Lesson
+                        Key</a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-word-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/teaching-and-learning/lecture-style/1-general-lessons-and-extensions/3-l1-teaching-learning-science/tl-l1-student-materials.docx']) }}">Student
+                        Materials</a>
+                </li>
+            </ul>
+            <p><strong>Extension C: Conducting Interviews with Children</strong></p>
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-word-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/teaching-and-learning/lecture-style/1-general-lessons-and-extensions/4-extc-conducting-interview-with-children/tl-extc-reading.docx']) }}">Reading
+                        (word)</a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-pdf-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/teaching-and-learning/lecture-style/1-general-lessons-and-extensions/4-extc-conducting-interview-with-children/tl-extc-reading.pdf']) }}">Reading
+                        (PDF)</a>
+                </li>
+                <li>
+                    <i class="fa-li fa fa-file-word-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/teaching-and-learning/lecture-style/1-general-lessons-and-extensions/4-extc-conducting-interview-with-children/tl-extc-quiz.docx']) }}">Quiz</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="card">
+    <div class="card-header" role="tab" id="headingTwo">
+        <h5 class="mb-0">
+            <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo"
+               aria-expanded="false" aria-controls="collapseTwo">Post-Introductory Cluster </a>
+        </h5>
+    </div>
+    <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo">
+        <div class="card-block">
+
+            <p><strong>Extension P: Cause and Effect</strong></p>
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/teaching-and-learning/lecture-style/1-general-lessons-and-extensions/5-extp-cause-effect/tl-extp-extension-mock-up.pptx']) }}">Extension
+                        Mock Up</a>
+                </li>
+            </ul>
+
+            <p><strong>Extension Q: Systems &amp; System Models / Matter &amp; Energy</strong></p>
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/teaching-and-learning/lecture-style/1-general-lessons-and-extensions/6-extq-systems-system-models-matter-energy/tl-extq-extension-mock-up.pptx']) }}">Extension
+                        Mock Up</a>
+                </li>
+            </ul>
+            <p><strong>Extension R: Stability & Change/Scale, Proportion & Quantity</strong></p>
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/teaching-and-learning/lecture-style/1-general-lessons-and-extensions/7-extr-stability-change-scale-proportion-quantity/tl-extr-extension-mock-up.pptx']) }}">Extension
+                        Mock Up</a>
+                </li>
+            </ul>
+            <p><strong>Extension S: Patterns</strong></p>
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-powerpoint-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/teaching-and-learning/lecture-style/1-general-lessons-and-extensions/8-exts-patterns/tl-exts-extension-mock-up.pptx']) }}">Extension
+                        Mock Up</a>
+                </li>
+            </ul>
+            <p><strong>Lesson 12: Teaching &amp; Learning Science II</strong></p>
+            <ul class="fa fa-ul">
+                <li>
+                    <i class="fa-li fa fa-file-word-o"></i>
+                    <a target="_blank"
+                       href="{{ route('file.show',['file' => 'web-docs/instructor/teaching-and-learning/lecture-style/1-general-lessons-and-extensions/9-l12-teaching-learning-science-ii/tl-l12-student-materials.docx']) }}">Student Materials</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
