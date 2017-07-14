e0e6ec376a3bc37c9f0fd6758e234de88c88bbeb
Henry Garcia <hjgarcia1@gmail.com>
2017-07-13 21:01:28 -0400

    don't track on dev sites unless stated

--- resources/views/layouts/partials/meta.blade.php
+++ resources/views/layouts/partials/meta.blade.php
@@ -2,7 +2,7 @@
 <meta http-equiv="X-UA-Compatible"
       content="IE=edge">
 <meta name="viewport"
-      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 <link rel="canonical"
       href="http://{{ URL::current() }}">
 <meta name="csrf-token"
@@ -10,6 +10,11 @@
 <meta name="description"
       content="IAT provides the resources and technology to support the implementation of successful Science, Technology, Engineering, and Mathematics (STEM) curricula.">
 
+@if(env('APP_TRACK') == 1)
+    <meta name="robots" content="all"/>
+    @else
+    <meta name="robots" content="noindex"/>
+@endif
 @if(isset($appError))
     <title>Next Gen PET | {{ $statusCode }}</title>
 @else
