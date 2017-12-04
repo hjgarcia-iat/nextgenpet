0beba9532daa844895206cf24d59b22ef8812271
Henry Garcia <hjgarcia1@gmail.com>
2017-12-04 16:01:12 -0500

    removed error from view if not present

--- resources/views/layouts/partials/_page-message.blade.php
+++ resources/views/layouts/partials/_page-message.blade.php
@@ -1,44 +1,46 @@
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            @if(Session::has('success'))
-                <div class="alert alert-success alert-dismissible"
-                     role="alert">
-                    <button type="button"
-                            class="close"
-                            data-dismiss="alert"
-                            aria-label="Close">
-                        <i class="fa fa-times"></i>
-                    </button>
-                    {{ session('success') }}
-                </div>
-            @endif
-            
-            @if (session('status'))
-                <div class="alert alert-success alert-dismissible"
-                     role="alert">
-                    <button type="button"
-                            class="close"
-                            data-dismiss="alert"
-                            aria-label="Close">
-                        <i class="fa fa-times"></i>
-                    </button>
-                    {{ session('status') }}
-                </div>
-            @endif
-            
-            @if(Session::has('error'))
-                <div class="alert alert-danger alert-dismissible"
-                     role="alert">
-                    <button type="button"
-                            class="close"
-                            data-dismiss="alert"
-                            aria-label="Close">
-                        <i class="fa fa-times"></i>
-                    </button>
-                    {{ session('error') }}
-                </div>
-            @endif
+@if(Session::has('success') || Session::has('status') || Session::has('error'))
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                @if(Session::has('success'))
+                    <div class="alert alert-success alert-dismissible"
+                         role="alert">
+                        <button type="button"
+                                class="close"
+                                data-dismiss="alert"
+                                aria-label="Close">
+                            <i class="fa fa-times"></i>
+                        </button>
+                        {{ session('success') }}
+                    </div>
+                @endif
+
+                @if (session('status'))
+                    <div class="alert alert-success alert-dismissible"
+                         role="alert">
+                        <button type="button"
+                                class="close"
+                                data-dismiss="alert"
+                                aria-label="Close">
+                            <i class="fa fa-times"></i>
+                        </button>
+                        {{ session('status') }}
+                    </div>
+                @endif
+
+                @if(Session::has('error'))
+                    <div class="alert alert-danger alert-dismissible"
+                         role="alert">
+                        <button type="button"
+                                class="close"
+                                data-dismiss="alert"
+                                aria-label="Close">
+                            <i class="fa fa-times"></i>
+                        </button>
+                        {{ session('error') }}
+                    </div>
+                @endif
+            </div>
         </div>
     </div>
-</div>
+@endif
