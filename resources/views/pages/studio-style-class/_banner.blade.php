78f08f13ccb71a1b5b0893954d7a3b743ae543b2
Henry Garcia <hjgarcia1@gmail.com>
2017-12-05 10:55:49 -0500

    banner images fixed

--- resources/views/pages/studio-style-class/_banner.blade.php
+++ resources/views/pages/studio-style-class/_banner.blade.php
@@ -1,11 +1,4 @@
-<div class="container carousel-container fill visible-md visible-lg">
-    <div id="myCarousel" class="carousel slide">
-        <div class="carousel-inner">
-            <div class="active item">
-                <div class="fill" style="background-image:url('/img/studio-slider.jpg');;">
-                    <div class="container"></div>
-                </div>
-            </div>
-        </div>
+<div class="container-fluid pl-0 pr-0">
+    <div class="fill" style="background-image:url('/img/studio-slider.jpg');;">
     </div>
 </div>
