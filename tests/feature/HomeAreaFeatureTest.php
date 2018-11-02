8959b77cc3f23926a9f3478cdb3a523895eedf49
Henry Garcia <hjgarcia1@gmail.com>
2018-11-01 21:56:23 -0400

    home area tested

---
+++ tests/feature/HomeAreaFeatureTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\Feature;
+
+use TestCase;
+
+/**
+ * Class HomeAreaFeatureTest
+ * @package Tests\Feature
+ */
+class HomeAreaFeatureTest extends TestCase
+{
+    public function test_we_can_see_the_home_page()
+    {
+        $this->visit('/')->assertResponseStatus(200);
+    }
+
+    public function test_we_can_see_the_module_and_units_page()
+    {
+        $this->visit('about/module-and-units')->assertResponseStatus(200);
+    }
+
+    public function test_we_can_see_the_overview_of_extensions_page()
+    {
+        $this->visit('about/overview-of-extensions')->assertResponseStatus(200);
+    }
+
+    public function test_we_can_see_the_overview_of_teaching_and_learning_materials_page()
+    {
+        $this->visit('about/overview-of-teaching-and-learning-activities')->assertResponseStatus(200);
+    }
+
+    public function test_we_can_see_the_overview_of_planning_and_conducting_page()
+    {
+        $this->visit('about/overview-of-pcim')->assertResponseStatus(200);
+    }
+
+    public function test_we_can_see_the_overview_of_engineering_design_activities_page()
+    {
+        $this->visit('about/engineering-design-activities')->assertResponseStatus(200);
+    }
+
+    public function test_we_can_see_the_ngss_and_next_gen_pet_page()
+    {
+        $this->visit('about/ngss-and-next-gen-pet')->assertResponseStatus(200);
+    }
+
+    public function test_we_can_see_the_implentation_versions_page()
+    {
+        $this->visit('about/implementation-versions')->assertResponseStatus(200);
+    }
+
+    public function test_we_can_see_the_about_the_development_staff_page()
+    {
+        $this->visit('about/development-staff')->assertResponseStatus(200);
+    }
+
+    public function test_we_can_see_the_faculty_online_learning_community_for_ngp_page()
+    {
+        $this->visit('about/faculty-online-learning')->assertResponseStatus(200);
+    }
+
+    public function test_we_can_see_the_terms_and_conditions_page()
+    {
+        $this->visit('terms-and-conditions')->assertResponseStatus(200);
+    }
+
+    public function test_we_can_see_the_privacy_policy_page()
+    {
+        $this->visit('privacy-policy')->assertResponseStatus(200);
+    }
+}
