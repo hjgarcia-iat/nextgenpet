a6741571a5cc13914071e732b66ffe228ebb62e9
Henry Garcia <hjgarcia1@gmail.com>
2018-11-05 21:43:44 -0500

    page messages now are across the page

--- resources/views/layouts/partials/_page-message.blade.php
+++ resources/views/layouts/partials/_page-message.blade.php
@@ -1,46 +1,46 @@
 @if(Session::has('success') || Session::has('status') || Session::has('error'))
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                @if(Session::has('success'))
-                    <div class="alert alert-success alert-dismissible"
-                         role="alert">
-                        <button type="button"
-                                class="close"
-                                data-dismiss="alert"
-                                aria-label="Close">
-                            <i class="fa fa-times"></i>
-                        </button>
-                        {{ session('success') }}
-                    </div>
-                @endif
 
-                @if (session('status'))
-                    <div class="alert alert-success alert-dismissible"
-                         role="alert">
-                        <button type="button"
-                                class="close"
-                                data-dismiss="alert"
-                                aria-label="Close">
-                            <i class="fa fa-times"></i>
-                        </button>
-                        {{ session('status') }}
-                    </div>
-                @endif
+    <div class="d-flex justify-content-center">
 
-                @if(Session::has('error'))
-                    <div class="alert alert-danger alert-dismissible"
-                         role="alert">
-                        <button type="button"
-                                class="close"
-                                data-dismiss="alert"
-                                aria-label="Close">
-                            <i class="fa fa-times"></i>
-                        </button>
-                        {{ session('error') }}
-                    </div>
-                @endif
+        @if(Session::has('success'))
+            <div class="alert alert-success alert-dismissible w-100"
+                 role="alert">
+                <button type="button"
+                        class="close"
+                        data-dismiss="alert"
+                        aria-label="Close">
+                    <i class="fa fa-times"></i>
+                </button>
+                <strong>{{ session('success') }}</strong>
             </div>
-        </div>
+        @endif
+
+        @if (session('status'))
+            <div class="alert alert-success alert-dismissible w-100"
+                 role="alert">
+                <button type="button"
+                        class="close"
+                        data-dismiss="alert"
+                        aria-label="Close">
+                    <i class="fa fa-times"></i>
+                </button>
+                <strong>{{ session('status') }}</strong>
+            </div>
+        @endif
+
+        @if(Session::has('error'))
+            <div class="alert alert-danger alert-dismissible w-100"
+                 role="alert">
+                <button type="button"
+                        class="close"
+                        data-dismiss="alert"
+                        aria-label="Close">
+                    <i class="fa fa-times"></i>
+                </button>
+                <strong>{{ session('error') }}</strong>
+            </div>
+        @endif
+
     </div>
+
 @endif
