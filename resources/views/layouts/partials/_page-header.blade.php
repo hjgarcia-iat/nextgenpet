389de629d8b484a44d301645f6734086d6e8d001
Henry Garcia <hjgarcia1@gmail.com>
2018-11-05 21:41:07 -0500

    refactored header partials

--- resources/views/layouts/partials/_page-header.blade.php
+++ resources/views/layouts/partials/_page-header.blade.php
@@ -1,62 +1,10 @@
 <header class="site-header">
-    <nav class="navbar navbar-dark bg-dark d-block d-md-none">
-        <div class="container">
-            <button type="button" class="navbar-toggler site-menu-toggle" href="#off-canvas">
-                <span class="fa fa-navicon"></span>
-            </button>
-        </div>
-    </nav>
-    <section class="top-bar d-none d-md-block">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-12 d-none d-md-flex justify-content-md-end align-items-md-center">
-                    <nav class="top-bar-navigation">
-                        <ul class="list-unstyled list-inline d-flex">
-                            <li><a href="#contact">Contact Us</a></li>
-                            <li><a target="_blank" href="http://shop.iat.com">Shop</a></li>
-                            <li><a href="tel:646-502-5231">646-502-5231</a></li>
-                            @if(auth()->check())
-                                <li>
-                                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button"
-                                       aria-haspopup="true" aria-expanded="false">{{ auth()->user()->name }}</a>
-                                    <div class="dropdown-menu">
-                                        <a class="dropdown-item" href="{{ route('my-account') }}">My Account</a>
-                                        <a class="dropdown-item" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
-                                           href="{{ route('login.destroy') }}">Logout
-                                        </a>
-                                        <form id="logout-form"
-                                              action="{{ route('login.destroy') }}"
-                                              method="POST"
-                                              style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </div>
-                                </li>
-                            @else
-                                <li><a href="{{ route('login.create') }}">Login/Register</a></li>
-                            @endif
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </div>
-    </section>
-
+    @include('layouts.partials._page-top-nav')
     @include('layouts.partials._page-message')
-
     <div class="container">
         <div class="row align-items-center just-content-end">
-            <div class="col-12 col-md-4">
-                <section class="page-logo text-center text-md-left">
-                    <a href="{{ url('/') }}" class="mx-auto">
-                        <img src="{{ asset('img/logo.png') }}" class="img-fluid" alt="Activate Learning">
-                    </a>
-                </section>
-            </div>
-            <div class="col-md-8 d-flex justify-content-end">
-                @include('layouts.partials._page-header-desktop-nav')
-            </div>
+            @include('layouts.partials._page-header-logo')
+            @include('layouts.partials._page-header-desktop-nav')
         </div>
     </div>
-
 </header>
