c774c391f770321b813686ac65126e967200139d
Henry Garcia <hjgarcia1@gmail.com>
2018-11-05 21:05:53 -0500

    refactored registration service

--- app/Services/RegistrationService.php
+++ app/Services/RegistrationService.php
@@ -22,42 +22,62 @@ class RegistrationService
      */
     public function register()
     {
-        $user = \DB::transaction(function () {
-            //create the user
-            $user = User::create([
-                'email'              => request()->get('register_email'),
-                'username'           => request()->get('register_email'),//needed for database
-                'account_status'     => 'Pending',
-                'user_group_id'      => 1,//needed for database
-                'account_expiration' => Carbon::now()->addYear(4),//needed for database
-                'order_number'       => 'NextGenPET',//needed for database
-            ]);
+        \DB::beginTransaction();
+        //create college
+        if(!$zip = Zip::whereZipCode(request()->get('zip'))->first()) \DB::rollBack();
 
-            $user->account()->create([
-                'first_name' => request()->get('first_name'),
-                'last_name'  => request()->get('last_name'),
-            ]);
+        if(!$state = State::whereAbbr($zip->state_prefix)->first()) \DB::rollBack();
+        //create college
+        $college = $this->createCollege($state, $zip);
+        //create the user
+        $user = $this->createUser();
+        //attach college to teacher
+        $user->colleges()->attach($college);
+        //attach the role
+        $user->assignRole('nextgen_pet_user');
 
-
-            //create college
-            $zip     = Zip::whereZipCode(request()->get('zip'))->first();
-            $state   = State::whereAbbr($zip->state_prefix)->first();
-            $college = College::create([
-                'name'     => request()->get('institution'),
-                'state_id' => $state->id,
-                'address'  => '-',
-                'city'     => $zip->city,
-                'zip'      => $zip->zip_code,
-            ]);
-
-            //attach college to teacher
-            $user->colleges()->attach($college);
-            //attach the role
-            $user->assignRole('nextgen_pet_user');
-
-            return $user;
-        });
+        \DB::commit();
 
         return $user;
+    }
+
+    /**
+     * Create user
+     * @return User
+     */
+    private function createUser()
+    {
+        $user = User::create([
+            'email'              => request()->get('register_email'),
+            'username'           => request()->get('register_email'),//needed for database
+            'account_status'     => 'Pending',
+            'user_group_id'      => 1,//needed for database
+            'account_expiration' => Carbon::now()->addYear(4),//needed for database
+            'order_number'       => 'NextGenPET',//needed for database
+        ]);
+
+        $user->account()->create([
+            'first_name' => request()->get('first_name'),
+            'last_name'  => request()->get('last_name'),
+        ]);
+        return $user;
+    }
+
+    /**
+     * Create College
+     * @param $state
+     * @param $zip
+     * @return College
+     */
+    private function createCollege($state, $zip)
+    {
+        $college = College::create([
+            'name'     => request()->get('institution'),
+            'state_id' => $state->id,
+            'address'  => '-',
+            'city'     => $zip->city,
+            'zip'      => $zip->zip_code,
+        ]);
+        return $college;
     }
 }
