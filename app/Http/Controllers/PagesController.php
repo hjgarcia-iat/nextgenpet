4c1c89f894d897d3595b36ccd9def9f4f245fbe1
Henry Garcia <hjgarcia1@gmail.com>
2018-11-05 20:15:35 -0500

    Page Model created and Page Controller refactored

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Page;
 
 /**
  * Class PagesController
@@ -22,82 +22,14 @@ class PagesController extends Controller
     /**
      * Show a Single Page based on path
      *
-     * @param Request $request
      * @return $this|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function show(Request $request)
+    public function show()
     {
-        if ($view = $this->is_valid_page($request)) {
+        if ($view = Page::isValidPage()) {
             return view('pages.' . $view);
         }
 
         abort(404);
-    }
-
-    /**
-     * @param Request $request
-     * @return string
-     */
-    private function is_valid_page(Request $request)
-    {
-        if ($this->is_partial($request->path())) {
-            abort(404);
-        }
-
-        if ($this->is_main_home_page($request->path())) {
-            return 'index';
-        }
-
-        if ($this->is_subdir_index($request)) {
-            return $request->path() . '.index';
-        }
-
-        if($this->is_sudir_view($request)) {
-            return $request->path();
-        }
-    }
-
-    /**
-     * Check if the view is a partial
-     *
-     * @param $path
-     * @return bool
-     */
-    private function is_partial($path)
-    {
-        return str_contains($path, '_') OR str_contains($path, 'partials');
-    }
-
-    /**
-     * Load main home page
-     *
-     * @param $path
-     * @return bool
-     */
-    private function is_main_home_page($path)
-    {
-        return $path == '/';
-    }
-
-    /**
-     * Check if the request is subdirectory home page
-     *
-     * @param Request $request
-     * @return bool
-     */
-    private function is_subdir_index(Request $request)
-    {
-        return \File::isDirectory(resource_path() . '/views/pages/' . $request->path()) and \File::exists(resource_path() . '/views/pages/' . $request->path() . '/index.blade.php');
-    }
-
-    /**
-     * Check if the requested view is subpage and not an index page
-     *
-     * @param Request $request
-     * @return bool
-     */
-    private function is_sudir_view(Request $request)
-    {
-        return \File::exists(resource_path() . '/views/pages/' . $request->path() . '.blade.php');
     }
 }
---
+++ app/Page.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App;
+
+/**
+ * Class Page
+ * @package App
+ */
+class Page
+{
+    /**
+     * Check if the page is valid
+     * @return string
+     */
+    public static function isValidPage()
+    {
+        if (self::isPartial(request()->path())) {
+            abort(404);
+        }
+
+        if (self::isMainHomePage(request()->path())) {
+            return 'index';
+        }
+
+        if (self::isSubDirectoryIndexView()) {
+            return request()->path() . '.index';
+        }
+
+        if(self::isSubDirectoryView()) {
+            return request()->path();
+        }
+    }
+
+    /**
+     * Check if the view is a partial
+     *
+     * @param $path
+     * @return bool
+     */
+    private static function isPartial($path)
+    {
+        return str_contains($path, '_') OR str_contains($path, 'partials');
+    }
+
+    /**
+     * Load main home page
+     *
+     * @param $path
+     * @return bool
+     */
+    private static function isMainHomePage($path)
+    {
+        return $path == '/';
+    }
+
+    /**
+     * Check if the request is subdirectory home page
+     *
+     * @return bool
+     */
+    private static function isSubDirectoryIndexView()
+    {
+        return \File::isDirectory(resource_path() . '/views/pages/' . request()->path()) and \File::exists(resource_path() . '/views/pages/' . request()->path() . '/index.blade.php');
+    }
+
+    /**
+     * Check if the requested view is subpage and not an index page
+     *
+     * @return bool
+     */
+    private static function isSubDirectoryView()
+    {
+        return \File::exists(resource_path() . '/views/pages/' . request()->path() . '.blade.php');
+    }
+}
