72f3947cf28d4664368df63bb6f3e166143a604e
Henry Garcia <hjgarcia1@gmail.com>
2021-06-24 20:19:33 -0400

    implementation-versions.blade.php changes

--- resources/views/pages/about/implementation-versions.blade.php
+++ resources/views/pages/about/implementation-versions.blade.php
@@ -12,30 +12,53 @@
 @section('content')
 
     <p>
-        <em>Next Gen PET</em>
-        is available in a
-        <em>Studio-style</em>
-        class version for small lab and discussion classroom environments of 12-36 students; and a
-        <em>Lecture-style</em>
-        class version for lecture-style classrooms. In both versions, students draw on evidence, discuss ideas, develop, test and modify models, and engage in practices of scientific argumentation.
+        <em>Next Gen PET</em> is available in a <em>Studio-style</em> class version for small lab and discussion
+        classroom environments of 12-36 students; and a <em>Lecture-style</em> class version for lecture-style
+        classrooms. In both versions, students draw on evidence, discuss ideas, develop, test and modify models, and
+        engage in practices of scientific argumentation.
     </p>
     <p>
-        The
-        <em>Studio-style</em>
-        class (SC) version is best suited for lower enrollment courses that meet ~5 hours/week and take place in a lab or workroom where students can work in groups around tables and perform experiments. In the SC version, students spend the majority of their time with their group of 3-4 peers making predictions, performing experiments, drawing conclusions, answering questions in a workbook, and discussing their ideas both within their group and with the whole class.
+        The <em>Studio Style</em> class (SC) version is best suited for lower enrollment courses that meet ~5 hours/week
+        and take place in a lab or workroom where students can work in groups around tables and perform experiments. In
+        the SC version, students spend the majority of their time with their group of 3-4 peers making predictions,
+        performing experiments, drawing conclusions, answering questions in a workbook, and discussing their ideas both
+        within their group and with the whole class.
     </p>
     <p>
-        The
-        <em>Lecture-style</em>
-        class (LC) version is best suited for courses that meet ~2.5 hours/week in lecture-style classrooms where students have desktops on which they can work. The LC version is suitable for courses with large enrollments, or in courses with smaller enrollments with limited meeting time and facilities. The content and pedagogical approach is similar to the SC version, but in the LC version: (1) students can do experiments requiring simple materials on their desktops as an option, but otherwise and for more extensive experiments, they watch videos; (2) students respond to questions (using hand-held response devices [e.g., “clickers”] or smart phones) to share ideas with the whole class; and (3) the instructor manages the class time through a set of PowerPoint™ slides that align with the student materials.
+        The <em>Lecture Style</em> class (LC) version is best suited for courses that meet ~2.5 hours/week in
+        lecture-style classrooms where students have desktops on which they can work. The LC version is suitable for
+        courses with large enrollments, or in courses with smaller enrollments with limited meeting time and facilities.
+        The content and pedagogical approach is similar to the SC version, but in the LC version: (1) students can do
+        experiments requiring simple materials on their desktops as an option, but otherwise and for more extensive
+        experiments, they watch videos; (2) students respond to questions (using hand-held response devices [e.g.,
+        “clickers”] or smart phones) to share ideas with the whole class; and (3) the instructor manages the class time
+        through a set of PowerPoint™ slides that align with the student materials.
+    </p>
+    <p>If your class format involves both lecture sessions and weekly laboratory meetings in a separate room, then you
+        can consider implementing a <em>hybrid version</em> of the curriculum: using lecture-style materials for the
+        lectures and studio-style materials for the lab periods.
     </p>
     <p>
-        If your class format involves both lecture sessions and weekly laboratory meetings in a separate room, then you can consider implementing a hybrid version of the curriculum: using lecture-style materials for the lectures and studio-style materials for the lab periods.  See the ‘Planning Your Course’ link on either the STUDIO STYLE CLASS page or the LECTURE STYLE CLASS page to see an example of a hybrid-style implementation.
-    </p>
-    <p>
-        The table below summarizes the differences between structural features of the two versions. To see examples of both versions, click on one of the following links: <strong>Developing Model of Magnetism</strong> (<a target="_blank" href="https://s3.amazonaws.com/nextgenpet/web-docs/review-activities/studio-style-class/module-mse/um-a3-student-materials.pdf">Studio Style</a>,
-        <a target="_blank" href="https://s3.amazonaws.com/nextgenpet/web-docs/review-activities/lecture-style-class/module-mse/um-l3-student-materials.pdf">Lecture Style</a>), <strong>Interactions and Forces</strong> (<a target="_blank" href="https://s3.amazonaws.com/nextgenpet/web-docs/review-activities/studio-style-class/module-fm/ufm-a1-student-materials.pdf">Studio Style</a>,
-        <a target="_blank" href="https://s3.amazonaws.com/nextgenpet/web-docs/review-activities/lecture-style-class/module-fm/ufm-l1-student-materials.pdf">Lecture Style</a>). See also the <strong>Design Principles</strong> table below that summarizes the differences in how each version implements the design features of the curriculum.
+        The table below summarizes the differences between structural features of the two versions. To see examples of
+        both versions, click on one of the following links: <strong>Developing Model of Magnetism</strong> (<a
+                target="_blank"
+                href="https://s3.amazonaws.com/nextgenpet/web-docs/review-activities/studio-style-class/module-mse/um-a3-student-materials.pdf">Studio
+            Style</a>,
+
+        <a target="_blank"
+                href="https://s3.amazonaws.com/nextgenpet/web-docs/review-activities/lecture-style-class/module-mse/um-l3-student-materials.pdf">Lecture
+            Style</a>),
+
+        <strong>Interactions and Forces</strong>
+
+        (<a target="_blank"
+                href="https://s3.amazonaws.com/nextgenpet/web-docs/review-activities/studio-style-class/module-fm/ufm-a1-student-materials.pdf">Studio
+            Style</a>,
+
+        <a target="_blank"
+                href="https://s3.amazonaws.com/nextgenpet/web-docs/review-activities/lecture-style-class/module-fm/ufm-l1-student-materials.pdf">Lecture
+            Style</a>). See also the <strong>Design Principles</strong> table below that summarizes the differences in
+        how each version implements the design features of the curriculum.
     </p>
 
     <table class="table table-bordered table-hover table-responsive">
@@ -85,8 +108,8 @@
         </tbody>
     </table>
     <p>
-        The following table shows how the design principles are put into in the studio-style class and lecture-style class versions of
-        <em>Next Gen PET</em>.
+        The following table shows how the design principles are put into in the studio-style class and lecture-style
+        class versions of <em>Next Gen PET</em>.
     </p>
     <table class="table table-bordered table-hover table-responsive">
         <thead>
@@ -104,16 +127,21 @@
         </tr>
         <tr>
             <td>2. Learning is a complex process.</td>
-            <td>Big ideas/science practices (e.g. writing explanations) developed within & across units. A variety of question types guide students’ thinking. Extensions (homework) are computer-based tutorials that include online quizzes
+            <td>Big ideas/science practices (e.g. writing explanations) developed within & across units. A variety of
+                question types guide students’ thinking. Extensions (homework) are computer-based tutorials that include
+                online quizzes
             </td>
-            <td>Similar to SC, except students do not write their own explanations; instead they evaluate carefully crafted explanations for accuracy. Extensions are the same as SC.
+            <td>Similar to SC, except students do not write their own explanations; instead they evaluate carefully
+                crafted explanations for accuracy. Extensions are the same as SC.
             </td>
         </tr>
         <tr>
             <td>3. Learning is facilitated through interaction with tools.</td>
-            <td>Students use hands-on materials, data acquisition tools and simulations, and answer questions on activity sheets.
+            <td>Students use hands-on materials, data acquisition tools and simulations, and answer questions on
+                activity sheets.
             </td>
-            <td>Students watch videos of experiments, demos and simulations, do simple hands-on activities (optional); answer questions with ‘clickers’ and on lesson sheets
+            <td>Students watch videos of experiments, demos and simulations, do simple hands-on activities (optional);
+                answer questions with ‘clickers’ and on lesson sheets
             </td>
         </tr>
         <tr>
@@ -122,11 +150,14 @@
             <td>Students discuss thinking w/ neighbors, limited sharing with whole class.</td>
         </tr>
         <tr>
-            <td>5. Learning is facilitated through establishment of certain specific behavioral practices and expectations.
+            <td>5. Learning is facilitated through establishment of certain specific behavioral practices and
+                expectations.
             </td>
-            <td>Written prompts/instructor comments support expectations of providing evidence, active participation, and responsibility for learning.
+            <td>Written prompts/instructor comments support expectations of providing evidence, active participation,
+                and responsibility for learning.
             </td>
-            <td>Similar to SC except the degree of participation is less and students are expected to reach consensus at end of each lesson.
+            <td>Similar to SC except the degree of participation is less and students are expected to reach consensus at
+                end of each lesson.
             </td>
         </tr>
         </tbody>
