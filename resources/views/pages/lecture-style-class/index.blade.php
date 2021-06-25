fb1b8ef91244b4c2c283156a9335cecf379c52e0
Henry Garcia <hjgarcia1@gmail.com>
2021-06-24 20:32:15 -0400

    homepage changes to style pages

--- resources/views/pages/lecture-style-class/index.blade.php
+++ resources/views/pages/lecture-style-class/index.blade.php
@@ -1,5 +1,4 @@
-@extends('layouts.two-column')
-@section('pageTitle','Lecture Style Class')
+@extends('layouts.two-column')@section('pageTitle','Lecture Style Class')
 @section('sidebar')
     @include('pages.lecture-style-class._sidebar')
 @stop
@@ -14,7 +13,9 @@
 
         <p>The Lecture-style class (LC) version is appropriate if:</p>
         <ul>
-            <li>Students sit in desks and do not have large tables where they can perform experiments (individual, movable desks are preferred to auditorium-style seating, but either will work. There are some optional chair-top experiments that students can perform if the materials are available.)
+            <li>Students sit in desks and do not have large tables where they can perform experiments (individual,
+                movable desks are preferred to auditorium-style seating, but either will work. There are some optional
+                chair-top experiments that students can perform if the materials are available.)
             </li>
             <li>
                 Enrollment is greater than 35 students (but lower enrollments are also suitable, for instance if the
@@ -28,14 +29,17 @@
             During each period the instructor uses PowerPoint slides to guide and pace the classroom lesson. Students
             work with nearest neighbors as they watch movies of experiments or simulations, perhaps perform simple
             desktop experiments, respond to clicker questions, and use worksheets to record their answers to questions
-            involving predictions, observations, making sense of data, and drawing conclusions. </p>
+            involving predictions, observations, making sense of data, and drawing conclusions.
+        </p>
         <p>
             You can use the links to the left to find out about many aspects of the lecture-style version of the
             curriculum, including many of the materials for instructors. A full set of instructor materials, including
-            answer keys, test banks, etc. is available from the login (password-protected) portion of this site. </p>
+            answer keys, test banks, etc. is available from the login (password-protected) portion of this site.
+        </p>
         <p>
             <strong>Pedagogy</strong> provides a description of the structure of each lesson and provides a detailed
-            example. </p>
+            example.
+        </p>
         <p>
             <strong>Equipment and Materials</strong> provides a list of optional materials that you can distribute to
             groups of students during some of the curriculum lessons so they can perform simple experiments on their
@@ -43,25 +47,48 @@
         </p>
         <p>
             <strong> Table of Contents with Time Estimates </strong> lists all the lessons in the curriculum (organized
-            by module and unit), along with an estimate of how much time each lesson might be expected to take. </p>
+            by module and unit), along with an estimate of how much time each lesson might be expected to take.
+        </p>
         <p>
-            <strong>Content Modules</strong> has a drop-down menu with links to each of the five content modules in the curriculum. Choosing a module takes you to a page showing graphic displays of the sequence of lessons and homework extensions for each unit in the module. When you click on any of the lessons or extensions in these graphics, a panel below opens that provides a brief description of the lesson or extension.</p>
+            <strong>Content Modules</strong> has a drop-down menu with links to each of the five content modules in the
+            curriculum. Choosing a module takes you to a page showing graphic displays of the sequence of lessons and
+            homework extensions for each unit in the module. When you click on any of the lessons or extensions in these
+            graphics, a panel below opens that provides a brief description of the lesson or extension.
+        </p>
 
         <p>
-            <strong>Teaching and Learning</strong> has a drop-down menu with links to a Table of Contents and the various clusters of Teaching and Learning lessons that are associated with the content modules.  Each of these links takes you to a page where you can see summaries of the lessons and extensions for that cluster.  You can download any of these lessons or extensions for use with your students.  They are free to use as you like.</p>
+            <strong>Teaching and Learning</strong> has a drop-down menu with links to a Table of Contents and the
+            various clusters of Teaching and Learning lessons that are associated with the content modules. Each of
+            these links takes you to a page where you can see summaries of the lessons and extensions for that cluster.
+            You can download any of these lessons or extensions for use with your students. They are free to use as you
+            like.
+        </p>
+
+        <p><strong>Planning Your Course</strong> provides guidelines on how to plan your quarter or semester-long course
+            using Next Gen PET materials.
+        </p>
+
+        <p><strong>Hybrid Implementation</strong>. If your class format involves both lecture sessions and weekly
+            laboratory meetings in a separate room, then you can consider implementing a hybrid version of the
+            curriculum, using lecture-style materials for the lectures and studio-style materials for the lab periods.
+        </p>
+
         <p>
             <strong>Extension Index</strong> takes you to a page where you can link to the homework extensions for each
             module and unit in the curriculum. Students work through each extension online and then (usually) go to
-            their institutions learning management system to take a quiz. </p>
+            their institutions learning management system to take a quiz.
+        </p>
         <p>
             <strong>Demonstration Movies and Simulations</strong> takes you to a page where you can link to the movies
             that instructors show during each lesson, or link to simulations used during some lessons. Students watch
             these movies during class and use them as a source of evidence to support their emerging ideas. The movies
-            are organized by module, unit, and lesson; each lesson page contains all the movies for that lesson. </p>
+            are organized by module, unit, and lesson; each lesson page contains all the movies for that lesson.
+        </p>
         <p>
             <strong>Classroom Videos</strong> takes you to a page where you can link to the large number of classroom
             video clips showing students engaged in both small group and whole class discussion. These clips are
-            organized by module, unit, and by lesson; each lesson page contains all the video clips for that lesson </p>
+            organized by module, unit, and by lesson; each lesson page contains all the video clips for that lesson
+        </p>
     </section>
 
 @stop
